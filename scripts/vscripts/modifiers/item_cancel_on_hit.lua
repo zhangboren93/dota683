fee6ab5968ecab7ba775152c26867af8a8f3af09
U-MS-MVTGEZKMUKTG\Administrator <bobo1993324@126.com>
2023-02-08 21:54:14 +0800

    hit cancels clarity & flash, hero's attack point and turn rate changes.

---
+++ scripts/vscripts/modifiers/item_cancel_on_hit.lua
@@ -0,0 +1,23 @@
+if modifier_cancels_item_on_hit == nil then
+    modifier_cancels_item_on_hit = class({})
+end
+function modifier_cancels_item_on_hit:GetAttributes()
+    return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+end
+
+function modifier_cancels_item_on_hit:DeclareFunctions()
+    local funcs = {
+        MODIFIER_EVENT_ON_TAKEDAMAGE
+    }
+    return funcs
+end
+
+function modifier_cancels_item_on_hit:IsHidden()
+    return true
+end
+
+function modifier_cancels_item_on_hit:OnTakeDamage()
+    local entity = self:GetParent()
+    entity:RemoveModifierByName("modifier_flask_healing")
+    entity:RemoveModifierByName("modifier_clarity_potion")
+end
