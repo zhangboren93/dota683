d0c0882b3406d77318c86ba1d5e6d8952171c057
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-02-24 19:09:24 +0800

    Tranquil attack cancels, fix bug with troll and rubick.

--- scripts/vscripts/modifiers/item_cancel_on_hit.lua
+++ scripts/vscripts/modifiers/item_cancel_on_hit.lua
@@ -7,7 +7,8 @@ end
 
 function modifier_cancels_item_on_hit:DeclareFunctions()
     local funcs = {
-        MODIFIER_EVENT_ON_TAKEDAMAGE
+        MODIFIER_EVENT_ON_TAKEDAMAGE,
+        MODIFIER_EVENT_ON_ATTACK_LANDED
     }
     return funcs
 end
@@ -22,4 +23,16 @@ function modifier_cancels_item_on_hit:OnTakeDamage()
     entity:RemoveModifierByName("modifier_clarity_potion")
     entity:RemoveModifierByName("modifier_bottle_regeneration")
     entity:RemoveModifierByName("modifier_rune_regen")
+end
+
+-- starts tranquil cooldown after attack lands
+function modifier_cancels_item_on_hit:OnAttackLanded(event)
+    local entity = self:GetParent()
+    if event.attacker ~= entity then 
+        return
+    end
+    local tranquil = entity:FindItemInInventory("item_tranquil_boots")
+    if tranquil ~= nil then
+        tranquil:StartCooldown(tranquil:GetSpecialValueFor("break_time"))
+    end
 end
