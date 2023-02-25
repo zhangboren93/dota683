3792a2a450ecf375fdd2990cc283ae066d2214fb
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-02-26 00:24:45 +0800

    implements data driven helm of the dominator.

--- scripts/vscripts/items/item_helm_of_the_dominator.lua
+++ scripts/vscripts/items/item_helm_of_the_dominator.lua
@@ -1,39 +1,17 @@
-if item_helm_of_the_dominator_modifier_lua == nil then
-    item_helm_of_the_dominator_modifier_lua = class({})
-end
-
-function item_helm_of_the_dominator_modifier_lua:GetAttributes()
-    return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
-end
-
-function item_helm_of_the_dominator_modifier_lua:OnCreated(kv)
---    print("item_orchid_regen_percentage_modifier:OnCreated")
-    self:StartIntervalThink(0.5)
-end
-
-function item_helm_of_the_dominator_modifier_lua:IsHidden()
-    return true
-end
-
-function item_helm_of_the_dominator_modifier_lua:OnIntervalThink()
-    --print("interval think")
-    local hParent = self:GetParent() --the unit.
-    if hParent == nil or hParent.FindItemInInventory == nil then
-        return
+function dominateCreep(event)
+    local item = event.ability
+    local caster = event.caster
+    local target = event.target
+    if item.dominatedCreep ~= nil and IsValidEntity(item.dominatedCreep) then
+        item.dominatedCreep:ForceKill(false)
     end
-    local item = hParent:FindItemInInventory("item_helm_of_the_dominator")
-    if item ~= nil and item:GetItemState() == 1 then
-        if not hParent:HasModifier("modifier_helm_damage_lifesteal_lua") then
-            print("Adding damage modifier " .. item:GetSpecialValueFor("bonus_damage"))
-            hParent:AddNewModifier(
-                hParent, nil, 
-                "modifier_helm_damage_lifesteal_lua",
-                { 
-                    bonus_damage = item:GetSpecialValueFor("bonus_damage"),
-                    lifesteal_percent = item:GetSpecialValueFor("lifesteal_percent")
-                })
-        end
-    else
-        hParent:RemoveModifierByName("modifier_helm_damage_lifesteal_lua")
-    end
+    local name = target:GetUnitName()
+    local spawn_location = target:GetAbsOrigin()
+    target:Destroy()
+
+	local double = CreateUnitByName( name, spawn_location, true, caster, caster:GetOwner(), caster:GetTeamNumber())
+	double:SetControllableByPlayer(caster:GetPlayerID(), true)
+    double:SetMaxHealth(double:GetMaxHealth() + item:GetSpecialValueFor("health_bonus"))
+    double:SetHealth(double:GetMaxHealth())
+    item.dominatedCreep = double
 end
