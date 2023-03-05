bec1709452b4dc6742ea9a97fd979c4bfbca7c53
U-MS-MVTGEZKMUKTG\Administrator <bobo1993324@126.com>
2023-03-05 12:49:17 +0800

    Disable maelstrom against tower.

--- scripts/vscripts/items/item_maelstrom.lua
+++ scripts/vscripts/items/item_maelstrom.lua
@@ -10,6 +10,10 @@ function modifier_item_maelstrom_datadriven_on_orb_impact(event)
     local victims = {}
     victims[new_target:GetEntityIndex()] = true
 
+    if new_target:IsBuilding() or new_target:IsMagicImmune() then
+        return
+    end
+
 	local particle = ParticleManager:CreateParticle( "particles/units/heroes/hero_zuus/zuus_arc_lightning_.vpcf", PATTACH_POINT_FOLLOW, target )
 	ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))   
 	ParticleManager:SetParticleControl(particle,1,Vector(new_target:GetAbsOrigin().x,new_target:GetAbsOrigin().y,new_target:GetAbsOrigin().z + new_target:GetBoundingMaxs().z ))
@@ -20,9 +24,9 @@ function modifier_item_maelstrom_datadriven_on_orb_impact(event)
     caster:SetThink(function()
         target = new_target
         new_target = nil
-		local units = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, chain_radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), 0, false)
+		local units = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, chain_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false)
         for i=1,#units do
-            if victims[units[i]:GetEntityIndex()] == nil then
+            if not units[i]:IsMagicImmune() and victims[units[i]:GetEntityIndex()] == nil then
                 new_target = units[i]
                 break
             end
