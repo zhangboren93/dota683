756b80c4135a608dae5bd177d33dd6a112318db9
U-MS-MVTGEZKMUKTG\Administrator <bobo1993324@126.com>
2023-04-09 19:43:32 +0800

    quelling blade kills ward, disable for rosh.

--- scripts/vscripts/items/item_quelling_blade.lua
+++ scripts/vscripts/items/item_quelling_blade.lua
@@ -5,13 +5,16 @@ function quelling_blade_kill_tree(keys)
     if target:GetClassname() == "ent_dota_tree" then
         target:CutDown(attacker:GetTeamNumber())
     end
+    if target:GetName() == "npc_dota_ward_base" or target:GetName() == "npc_dota_ward_base_truesight" then
+        target:Kill(ability, attacker)
+    end
 end
 
 function quelling_blade_attack_start(event)
     local target = event.target
     local attacker = event.attacker
     local ability = event.ability
-    if target:IsCreep() and target:GetTeamNumber() ~= attacker:GetTeamNumber() then
+    if target:IsCreep() and target:GetTeamNumber() ~= attacker:GetTeamNumber() and target:GetName() ~= "npc_dota_roshan" then
         if attacker:IsRangedAttacker() then
             ability:ApplyDataDrivenModifier(attacker, attacker, "item_quelling_blade_active_ranged_modifier", {})
         else
@@ -19,7 +22,7 @@ function quelling_blade_attack_start(event)
             ability:ApplyDataDrivenModifier(attacker, attacker, "item_quelling_blade_active_melee_modifier", {})
         end
     else
-        ability:RemoveModifierByName("item_quelling_blade_active_ranged_modifier")
-        ability:RemoveModifierByName("item_quelling_blade_active_melee_modifier")
+        attacker:RemoveModifierByName("item_quelling_blade_active_ranged_modifier")
+        attacker:RemoveModifierByName("item_quelling_blade_active_melee_modifier")
     end
 end
