46cece8d830e2cdeddce994b08f44e672b31acda
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-04-25 13:36:49 +0800

    OD ult applies to imprisoned unit

--- scripts/vscripts/heroes/hero_obsidian_destroyer/astral_imprisonment.lua
+++ scripts/vscripts/heroes/hero_obsidian_destroyer/astral_imprisonment.lua
@@ -3,7 +3,7 @@ function handleAbilityExecuted(keys)
     local ability = keys.ability
     local event_ability = keys.event_ability
     local target = keys.target
-    if event_ability:GetName() == "obsidian_destroyer_astral_imprisonment" then
+    if event_ability:GetName() == "obsidian_destroyer_astral_imprisonment" and target:IsRealHero() then
 		ability:SetLevel(event_ability:GetLevel())
 		ability:ApplyDataDrivenModifier(unit, target, "modifier_od_imprison_int_steal", {})
 		ability:ApplyDataDrivenModifier(unit, unit,   "modifier_od_imprison_int_gain", {})
--- scripts/vscripts/heroes/hero_obsidian_destroyer/sanity_eclipse.lua
+++ scripts/vscripts/heroes/hero_obsidian_destroyer/sanity_eclipse.lua
@@ -16,28 +16,40 @@ function SanityEclipseDamage( keys )
 	local threshold = ability:GetLevelSpecialValueFor("int_threshold", (ability:GetLevel() -1))
 
 	
+	local damageDelay = 0
+	if target:IsInvulnerable() or target:IsOutOfGame() then
+		print(target:GetName() .. " is out of the game")
+		if not target:HasModifier("modifier_obsidian_destroyer_astral_imprisonment_prison") then
+			print("not imprison")
+			return
+		end
+		damageDelay = target:FindModifierByName("modifier_obsidian_destroyer_astral_imprisonment_prison"):GetRemainingTime() + 0.1
+		print(damageDelay)
+	end
 
-	local damage_table = {} 
+	
+	target:SetThink(function()
+		local damage_table = {} 
 
-	damage_table.attacker = caster
-	damage_table.damage_type = ability:GetAbilityDamageType()
-	damage_table.ability = ability
-	damage_table.victim = target
+		damage_table.attacker = caster
+		damage_table.damage_type = ability:GetAbilityDamageType()
+		damage_table.ability = ability
+		damage_table.victim = target
 
-	--if od's int is lower than targets int then keep do nothing and keep targets mana as it is
-	if od_int < target_int then
-		target:SetMana(mana)
-		--if the int difference is below or equal to threshold, burn 75% current mana and apply int difference * damage_modifier in magic damage
-		elseif 
-			(od_int - target_int) < threshold or (od_int - target_int) == threshold then
-		target:SetMana(mana*0.25)
-		damage_table.damage = (od_int - target_int) * dmg_multiplier
-		ApplyDamage(damage_table)
-		--if the int difference is bigger than than threshold then deal damage
+		--if od's int is lower than targets int then keep do nothing and keep targets mana as it is
+		if od_int < target_int then
+			target:SetMana(mana)
+			--if the int difference is below or equal to threshold, burn 75% current mana and apply int difference * damage_modifier in magic damage
+		elseif (od_int - target_int) < threshold or (od_int - target_int) == threshold then
+			target:SetMana(mana*0.25)
+			damage_table.damage = (od_int - target_int) * dmg_multiplier
+			ApplyDamage(damage_table)
+			--if the int difference is bigger than than threshold then deal damage
 		elseif 	(od_int - target_int) > threshold then
 			damage_table.damage = (od_int - target_int)	* dmg_multiplier
 			ApplyDamage(damage_table)
-	end
+		end
+	end, "sanity eclipse", damageDelay)
 end
 
 
