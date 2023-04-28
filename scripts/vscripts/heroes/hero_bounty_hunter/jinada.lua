fd0b2dfffa7a58c07d5214f524202f0579dfc65c
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-04-28 08:09:47 +0800

    fix jinada works on deny, illusion and buildings.

--- scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
+++ scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
@@ -10,9 +10,20 @@ function Jinada( keys )
 
 	ability:StartCooldown(cooldown)
 
-	caster:RemoveModifierByName(modifierName) 
+--	caster:RemoveModifierByName(modifierName) 
+--
+--	caster:SetThink(function()
+--		ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
+--		end, "bounty jinada", cooldown)	
+end
 
-	caster:SetThink(function()
-		ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
-		end, "bounty jinada", cooldown)	
+function handleAttackStart(event)
+	local ability = event.ability
+	local attacker = event.attacker
+	local target = event.target
+	if attacker:IsRealHero() and not target:IsBuilding() and ability:IsCooldownReady() and target:GetTeam() ~= attacker:GetTeam() then
+		ability:ApplyDataDrivenModifier(attacker, attacker, "modifier_jinada_datadriven", {})
+	else
+		attacker:RemoveModifierByName("modifier_jinada_datadriven")
+	end
 end
