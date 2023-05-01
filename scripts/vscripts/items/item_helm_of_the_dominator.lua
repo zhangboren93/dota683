9e84255f2e934a0b50b112fe132709e360e50639
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-05-01 12:35:30 +0800

    Fix helm of dom creep not inheriting lycan's passive.

--- scripts/vscripts/items/item_helm_of_the_dominator.lua
+++ scripts/vscripts/items/item_helm_of_the_dominator.lua
@@ -1,19 +1,19 @@
 function dominateCreep(event)
-    local item = event.ability
-    local caster = event.caster
-    local target = event.target
-    if item.dominatedCreep ~= nil and IsValidEntity(item.dominatedCreep) then
-        item.dominatedCreep:ForceKill(false)
-    end
-    local name = target:GetUnitName()
-    local spawn_location = target:GetAbsOrigin()
-    target:ForceKill(false)
+	local item = event.ability
+	local caster = event.caster
+	local target = event.target
+	if item.dominatedCreep ~= nil and IsValidEntity(item.dominatedCreep) then
+		item.dominatedCreep:ForceKill(false)
+	end
+	local name = target:GetUnitName()
+	local spawn_location = target:GetAbsOrigin()
+	target:ForceKill(false)
 
-	local double = CreateUnitByName( name, spawn_location, true, caster, caster:GetOwner(), caster:GetTeamNumber())
+	local double = CreateUnitByName( name, spawn_location, true, caster, caster, caster:GetTeamNumber())
 	double:SetControllableByPlayer(caster:GetPlayerID(), true)
-    double:SetThink(function()
-        double:SetMaxHealth(double:GetMaxHealth() + 500)
-        double:Heal(500, caster)
-    end, "health later", 0.2)
-    item.dominatedCreep = double
+	double:SetThink(function()
+		double:SetMaxHealth(double:GetMaxHealth() + 500)
+		double:Heal(500, caster)
+	end, "health later", 0.2)
+	item.dominatedCreep = double
 end
