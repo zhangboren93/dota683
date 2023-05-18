5a0a61cc8d4c76cf631db0ce07b89c3c448a5629
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-05-18 08:16:32 +0800

    pugna allie decrepify magic resist debuf.

--- scripts/vscripts/hero_innate_abilities.lua
+++ scripts/vscripts/hero_innate_abilities.lua
@@ -5,3 +5,4 @@ hero_innate_abilities["npc_dota_hero_winter_wyvern"] = "winter_wyvern_arctic_bur
 hero_innate_abilities["npc_dota_hero_clinkz"] = "clinkz_attack_animation_datadriven"
 hero_innate_abilities["npc_dota_hero_silencer"] = "silencer_global_silence_aghs_datadriven"
 hero_innate_abilities["npc_dota_hero_spirit_breaker"] = "spirit_breaker_empowering_haste_activate_debuff_datadriven"
+hero_innate_abilities["npc_dota_hero_pugna"] = "pugna_decrepify_ally_debuff_datadriven"
