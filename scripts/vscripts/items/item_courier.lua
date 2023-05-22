f7979dfc04ec65c7cfdb630c029dccc36279ce80
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-05-22 13:50:26 +0800

    Flying courier visual.

--- scripts/vscripts/items/item_courier.lua
+++ scripts/vscripts/items/item_courier.lua
@@ -1,5 +1,6 @@
 function enableCourier(event)
     local caster = event.caster
+   -- caster:GetPlayerOwner():SpawnCourierAtPosition(caster:GetAbsOrigin()):Destroy()
     caster:GetPlayerOwner():SpawnCourierAtPosition(caster:GetAbsOrigin())
 end
 
@@ -16,6 +17,7 @@ function flyingUpgradeChecker(event)
     local entity = event.caster
     local ability = event.ability
     if entity:HasModifier("modifier_courier_flying_upgrade_active") then
+        entity:SetModel("models/props_gameplay/donkey_wings.vmdl")
         return
     end
     if entity:HasItemInInventory("item_flying_courier_datadriven") then
@@ -23,5 +25,12 @@ function flyingUpgradeChecker(event)
         entity:SetBaseMaxHealth(150)
         entity:RemoveItem(entity:FindItemInInventory("item_flying_courier_datadriven"))
         entity:SetMoveCapability(DOTA_UNIT_CAP_MOVE_FLY)
+        entity:SetModel("models/props_gameplay/donkey_wings.vmdl")
+        entity:RemoveModifierByName("modifier_courier_ground_visual")
+    else
+        entity:SetModel("models/props_gameplay/donkey.vmdl")
+        if not entity:HasModifier("modifier_courier_ground_visual") then
+            ability:ApplyDataDrivenModifier(entity, entity, "modifier_courier_ground_visual", {})
+        end
     end
 end
