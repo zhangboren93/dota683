9c0a53b971b458e148bc8c6a3368a7bf5d4de79b
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-05-23 08:24:03 +0800

    F2 select current team's courier.

--- panorama/scripts/custom_game/key_bindings.js
+++ panorama/scripts/custom_game/key_bindings.js
@@ -1,11 +1,17 @@
 function OnCustomeGameSelectCourier()
 {
-  $("#courier_shared_label").text="sd12eqwsdfasf"
+    var entities = Entities.GetAllEntitiesByName("npc_dota_courier");
+    entities.forEach(element => {
+        if (Entities.GetTeamNumber(element) == Players.GetTeam(Players.GetLocalPlayer())) {
+            GameUI.SelectUnit(element, false);
+        }
+    });
+    //$("#courier_shared_label").text = entities.length + " " + Players.GetLocalPlayer() + " " + Players.GetTeam(Players.GetLocalPlayer());
 }
 
 function OnCustomGameCourierSend()
 {
-  $("#courier_shared_label").text="4322523354"
+    //$("#courier_shared_label").text="4322523354";
 }
 
 Game.AddCommand( "CustomGameSelectCourier", OnCustomeGameSelectCourier, "", 0 );
