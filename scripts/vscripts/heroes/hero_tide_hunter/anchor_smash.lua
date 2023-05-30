60c9fb66aaeb8f1f0fa8da3adae84b8603e926e5
U-DESKTOP-8IS377P\PC <bobo1993324@126.com>
2023-05-30 20:25:32 +0800

    tide hunter anchor smash pierce spell immunity.

---
+++ scripts/vscripts/heroes/hero_tide_hunter/anchor_smash.lua
@@ -0,0 +1,17 @@
+function handleSpellStart(event)
+	local caster = event.caster
+	local ability = event.ability
+	local ability_range = ability:GetCastRange(nil, nil)
+	local units = FindUnitsInRadius(
+		caster:GetTeam(), 
+		caster:GetAbsOrigin(), nil, 
+		ability_range, 
+		DOTA_UNIT_TARGET_TEAM_ENEMY, 
+		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+		FIND_ANY_ORDER, false)
+	for i=1,#units do
+		ApplyDamage({victim = units[i], attacker = caster, damage = ability:GetAbilityDamage(), damage_type = DAMAGE_TYPE_PHYSICAL})
+		ability:ApplyDataDrivenModifier(caster, units[i], "modifier_tidehunter_anchor_smash_datadriven", {})
+	end
+end
