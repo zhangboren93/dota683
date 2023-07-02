36b65ed683b38c7566730e412230300ecc64d261
Boren Zhang <bobo1993324@126.com>
2023-07-02 11:02:02 +0800

    Sven scepter

--- scripts/vscripts/units/ability_execute_buff.lua
+++ scripts/vscripts/units/ability_execute_buff.lua
@@ -4,7 +4,11 @@ function handleAbilityExecuted(keys)
 	local event_ability = keys.event_ability
 	local target_ability = keys.TargetAbility
 	local modifier = keys.Modifier
+	local requires_scepter = keys.RequiresScepter
 	if event_ability:GetName() == target_ability then
+		if requires_scepter == 1 and not unit:HasScepter() then
+			return
+		end
 		ability:SetLevel(event_ability:GetLevel())
 		ability:ApplyDataDrivenModifier(unit, unit, modifier, {})
 	end
