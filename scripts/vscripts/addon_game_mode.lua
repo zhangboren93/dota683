fdfab4ed7c580de70da0035e3c3d246fce29ecb3
U-MS-MVTGEZKMUKTG\Administrator <bobo1993324@126.com>
2023-07-16 19:19:15 +0800

    Ogre ignite description.

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1219,23 +1219,6 @@ function CAddonTemplateGameMode:ModifierGainedFilter(event)
 				end
 			end, "nightmare_damage later", 1.5)
 		end
-	elseif event.name_const == "modifier_ogre_magi_ignite" then
-		local parent = EntIndexToHScript(event.entindex_parent_const)
-		local caster = EntIndexToHScript(event.entindex_caster_const)
-		local ability_multicast = caster:FindAbilityByName("ogre_magi_multicast")
-		local ability_ignite_splash = caster:FindAbilityByName("ogre_magi_multicast_bonus_datadriven")
-		ability_ignite_splash:ApplyDataDrivenModifier(caster, parent, "modifier_ignite_no_ex_duration_checker", {})
-		if ability_multicast:GetLevel() > 0 and not parent:HasModifier("modifier_ogre_magi_ignite_splash") then
-			local ability_ignite = caster:FindAbilityByName("ogre_magi_ignite")
-			local ignite_radius = ability_multicast:GetSpecialValueFor("ignite_range")
-			local units = FindUnitsInRadius(caster:GetTeam(), parent:GetAbsOrigin(), nil, ignite_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, 
-					DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, FIND_ANY_ORDER, false)
-			print("ignite splashes " .. #units)
-			for i=1,#units do
-				ability_ignite_splash:ApplyDataDrivenModifier(caster, units[i], "modifier_ogre_magi_ignite_splash", { duration = 1})
-				units[i]:AddNewModifier(caster, ability_ignite, "modifier_ogre_magi_ignite", { duration = ability_ignite:GetSpecialValueFor("duration")})
-			end
-		end
 	elseif event.name_const == "modifier_naga_siren_ensnare" then
 		local parent = EntIndexToHScript(event.entindex_parent_const)
 		if parent:IsMagicImmune() then
