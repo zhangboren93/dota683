b5e93107917cc53c7f062852aa9d35745f008a5f
Boren Zhang <bobo1993324@gmail.com>
2023-07-17 20:51:53 +0800

    Transfer nightmare when bane attacks.

--- scripts/vscripts/heroes/hero_bane/nightmare.lua
+++ scripts/vscripts/heroes/hero_bane/nightmare.lua
@@ -5,3 +5,15 @@ function handleIntervalThink(event)
 		ApplyDamage({victim = target, attacker = caster, damage = 20, damage_type = DAMAGE_TYPE_PURE})
 	end
 end
+
+function handleAttackStart(event)
+	local target = event.target
+	local attacker = event.attacker
+	local caster = event.caster
+	local nightmare = caster:FindAbilityByName("bane_nightmare")
+	if target:HasModifier("modifier_bane_nightmare") and target:GetTeam() ~= attacker:GetTeam() then
+		attacker:AddNewModifier(caster, nightmare, "modifier_bane_nightmare",
+			{duration = nightmare:GetSpecialValueFor("duration")})
+		target:RemoveModifierByName("modifier_bane_nightmare")
+	end
+end
