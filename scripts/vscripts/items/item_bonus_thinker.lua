e383fcf3cfd1c23dc845d2473d6a2ee4669355ce
Violet Nonbloosom <silkqcek@yeah.net>
2023-08-10 02:29:55 +0000

    Detail bug fix & Aquila rework
    Signed-off-by: Violet Nonbloosom <silkqcek@yeah.net>

--- scripts/vscripts/items/item_bonus_thinker.lua
+++ scripts/vscripts/items/item_bonus_thinker.lua
@@ -18,7 +18,6 @@ item2pctregen["item_bloodstone_datadriven"] = 200
 function handleIntervalThink(event)
 	local caster = event.caster
 	local ability = event.ability
-	item2modifier("item_ring_of_aquila",	"modifier_ring_of_aquila_bonus_armor", caster, ability)
 	item2modifier("item_maelstrom", 		"modifier_maelstrom_bonus_attack_speed", caster, ability)
 	item2modifier("item_shadow_amulet", 	"modifier_item_shadow_amulet_attack_speed", caster, ability)
 	item2modifier("item_soul_ring", 		"modifier_item_soul_ring_health_regen", caster, ability)
@@ -47,7 +46,7 @@ function handleIntervalThink(event)
 end
 
 function item2modifier(itemname, modifier, caster, ability)
-	local item = caster:FindItemInInventory(itemname)
+	local item = caster:FindItemInInventory(itemname)	
 	if item ~= nil and item:GetItemState() == 1  then
 		if not caster:HasModifier(modifier) then
 			ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
