99752f66c26a2e57caf8fac7c40220d7cca7908d
Boren Zhang <bobo1993324@126.com>
2023-08-23 08:45:52 +0800

    refactor heart interaction with roshan.

--- scripts/vscripts/items/item_heart.lua
+++ scripts/vscripts/items/item_heart.lua
@@ -6,7 +6,9 @@
 	Additional parameters: keys.CooldownMelee
 ================================================================================================================= ]]
 function modifier_item_heart_datadriven_regen_on_take_damage(keys)
-	if keys.attacker:IsRealHero() or keys.attacker:IsControllableByAnyPlayer() then
+	if keys.attacker:IsRealHero() or keys.attacker:IsControllableByAnyPlayer() 
+		or keys.attacker:HasModifier("roshan_inherent_buffs_checker_datadriven") then
+
 		if keys.caster:IsRangedAttacker() then
 			keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel()))
 		else  --If the caster is melee.
