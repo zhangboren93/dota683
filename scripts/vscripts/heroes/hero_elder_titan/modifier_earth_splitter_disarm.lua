780b6654f45033685fff439faadbad3a3fea8e19
Violet-Nonbloosom <silkqcek@yeah.net>
2023-10-10 10:31:31 +0800

    Add scepter effect to Elder Titan.

---
+++ scripts/vscripts/heroes/hero_elder_titan/modifier_earth_splitter_disarm.lua
@@ -0,0 +1,29 @@
+modifier_elder_titan_earth_splitter_disarm = class({})
+
+function modifier_elder_titan_earth_splitter_disarm:OnCreated(kv)
+    self.kv = kv
+end
+
+function modifier_elder_titan_earth_splitter_disarm:GetAttributes()
+    return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+end
+
+function modifier_elder_titan_earth_splitter_disarm:CheckState()
+	local state = 
+    {
+	    [MODIFIER_STATE_DISARMED] = true,
+	}
+	return state
+end
+
+function modifier_elder_titan_earth_splitter_disarm:IsDebuff()
+    return true
+end
+
+function modifier_elder_titan_earth_splitter_disarm:GetEffectName()
+	return "particles/units/heroes/hero_elder_titan/elder_titan_scepter_disarm.vpcf"
+end
+
+function modifier_elder_titan_earth_splitter_disarm:GetEffectAttachType()
+	return PATTACH_OVERHEAD_FOLLOW
+end
