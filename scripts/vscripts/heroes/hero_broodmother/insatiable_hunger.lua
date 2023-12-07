a6de0f3233822b514c314638c0a49e82195cd372
Boren Zhang <bobo1993324@gmail.com>
2023-12-07 22:55:27 +0800

    Remake insatiable hunger, dispellable.

--- scripts/vscripts/heroes/hero_broodmother/insatiable_hunger.lua
+++ scripts/vscripts/heroes/hero_broodmother/insatiable_hunger.lua
@@ -1,12 +1,9 @@
-function handleAbilityExecuted(event)
-	local event_ability = event.event_ability
-	if event_ability:GetName() == "broodmother_insatiable_hunger" then
-		local ability = event.ability
-		local caster = event.caster
-		local bonus_damage = event_ability:GetSpecialValueFor("bonus_damage_tooltip")
-		local modifier_count = bonus_damage / 10
-		for i=1,modifier_count do
-			ability:ApplyDataDrivenModifier(caster, caster, "modifier_insatiable_hunger_damage_datadriven", {})
-		end
-	end
+--[[Author: Pizzalol
+	Date: 14.01.2015.
+	Stops the sound upon expiration]]
+function InsatiableHungerStopSound( keys )
+	local caster = keys.caster
+	local sound = keys.sound
+
+	StopSoundEvent(sound, caster)
 end
