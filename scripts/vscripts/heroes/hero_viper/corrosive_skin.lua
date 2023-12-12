e9775262ec21892b422f7a0fcb838c21c2b2a34d
Boren Zhang <bobo1993324@126.com>
2023-12-12 13:34:59 +0800

    Remake viper's corrosive skin, pierce spell immunity.

---
+++ scripts/vscripts/heroes/hero_viper/corrosive_skin.lua
@@ -0,0 +1,11 @@
+function handleTakeDamage(event)
+	local caster = event.caster
+	local attacker = event.attacker
+	local ability = event.ability
+	local range = ability:GetSpecialValueFor("max_range_tooltip")
+	if attacker:GetTeam() ~= caster:GetTeam() 
+		and (attacker:GetAbsOrigin() - caster:GetAbsOrigin()):Length2D() <= range then
+		ability:ApplyDataDrivenModifier(caster, attacker, "modifier_corrosive_skin_debuff_datadriven", {})
+		attacker:EmitSound("Hero_Viper.CorrosiveSkin")
+	end
+end
