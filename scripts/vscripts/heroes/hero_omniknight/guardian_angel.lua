4497def91cc4bf9882d9f19e30f0333f77eff7bf
Boren Zhang <bobo1993324@126.com>
2023-12-17 19:05:38 +0800

    Remake omniknight guardian angel.

---
+++ scripts/vscripts/heroes/hero_omniknight/guardian_angel.lua
@@ -0,0 +1,30 @@
+function handleSpellStart(event)
+	local caster = event.caster
+	local ability = event.ability
+	if caster:HasScepter() then
+		buildings = FindUnitsInRadius(
+			caster:GetTeam(),
+			caster:GetAbsOrigin(), 
+			nil,
+			20000,
+			DOTA_UNIT_TARGET_TEAM_FRIENDLY,
+			DOTA_UNIT_TARGET_BUILDING,
+			DOTA_UNIT_TARGET_FLAG_INVULNERABLE,
+			FIND_ANY_ORDER,
+			false)
+		for i=1,#buildings do
+			ability:ApplyDataDrivenModifier(caster, buildings[i], "modifier_guardian_angel", {})
+			buildings[i].guardian_angel_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_omniknight/omniknight_guardian_angel_ally.vpcf",
+				PATTACH_ABSORIGIN, buildings[i])
+		end
+	end
+end
+
+function handleDestroy(event)
+	local target = event.target
+	if target.guardian_angel_particle ~= nil then
+		ParticleManager:DestroyParticle(target.guardian_angel_particle, false)
+		ParticleManager:ReleaseParticleIndex(target.guardian_angel_particle)
+		target.guardian_angel_particle = nil
+	end
+end
