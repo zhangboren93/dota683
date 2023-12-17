07fc83e7bcb55bd4913d5ba0babbbdaced60229b
Boren Zhang <bobo1993324@126.com>
2023-12-17 23:44:29 +0800

    Gyro calldown remake

---
+++ scripts/vscripts/heroes/hero_gyrocopter/call_down.lua
@@ -0,0 +1,42 @@
+--[[Author: YOLOSPAGHETTI
+	Date: March 28, 2016
+	Gives the caster's team vision in the radius]]
+function GiveVision(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local point = keys.target_points[1]
+	local sight_radius = ability:GetLevelSpecialValueFor("vision_radius", (ability:GetLevel() -1))
+	local sight_duration = ability:GetLevelSpecialValueFor("vision_duration", (ability:GetLevel() -1))
+	
+	AddFOWViewer(caster:GetTeam(), point, sight_radius, sight_duration, false)
+end
+
+function handleDestroyed(event)
+	local target = event.target
+	local modifier = event.Modifier
+	local caster = event.caster
+	local ability = event.ability
+	local damage = event.Damage
+	local radius = ability:GetSpecialValueFor("radius")
+	local units = FindUnitsInRadius(
+		caster:GetTeam(),
+		target:GetAbsOrigin(),
+		nil,
+		radius,
+		DOTA_UNIT_TARGET_TEAM_ENEMY,
+		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP,
+		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+		FIND_ANY_ORDER,
+		false)
+	for i=1,#units do
+		if not units[i]:IsMagicImmune() then
+			ApplyDamage({
+				victim = units[i],
+				attacker = caster,
+				damage = damage,
+				damage_type = DAMAGE_TYPE_MAGICAL,
+				ability = ability})
+		end
+		ability:ApplyDataDrivenModifier(caster, units[i], modifier, {})
+	end
+end
