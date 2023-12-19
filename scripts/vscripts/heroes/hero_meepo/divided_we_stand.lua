5fdd8fd513c518b4a8964943fff9715ca5d98f4f
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2023-12-19 09:41:04 +0800

    Meepo Clone tranquil boots derive.

--- scripts/vscripts/heroes/hero_meepo/divided_we_stand.lua
+++ scripts/vscripts/heroes/hero_meepo/divided_we_stand.lua
@@ -18,19 +18,74 @@ function checkAghs(keys)
             ApplyDebufModifiers(caster, ability, "modifier_dws_intellect_datadriven", 
                 caster.mainMeepo:GetIntellect() - caster.mainMeepo:GetBaseIntellect(), "item_power_treads")
         end
-        --TODO boots at the same location
-        if caster.mainMeepo:HasItemInInventory("item_travel_boots_datadriven") 
-            and not caster.mainMeepo:HasItemInInventory("item_power_treads")
-            and not caster.mainMeepo:HasItemInInventory("item_phase_boots")
-            and not caster:HasItemInInventory("item_travel_boots_datadriven") then
-            caster.travelboot = caster:AddItemByName("item_travel_boots_datadriven")
+
+        --rewrite from DOTA_IMBA meepo
+
+        local rewrite_boots = {
+            "item_travel_boots_datadriven",
+            "item_tranquil_boots_datadriven",
+        }
+
+        local all_boots = {
+            "item_travel_boots_datadriven",
+            "item_power_treads",
+            "item_arcane_boots",
+            "item_phase_boots",
+            "item_tranquil_boots_datadriven",
+            "item_boots",
+        }
+
+        local ignore_custom_boots = false
+        for _, boots in pairs(all_boots) do
+            if caster:HasItemInInventory(boots) then
+                ignore_custom_boots = true
+                break
+            end
         end
-        if (not caster.mainMeepo:HasItemInInventory("item_travel_boots_datadriven")
-            or caster.mainMeepo:HasItemInInventory("item_power_treads")
-            or caster.mainMeepo:HasItemInInventory("item_phase_boots")) and caster.travelboot ~= nil then
-            caster:RemoveItem(caster.travelboot)
-            caster.travelboot = nil
+
+        local rewrite_found = false
+        if ignore_custom_boots == false then
+            local prime = caster:GetCloneSource()
+            for _, boots_name in pairs(rewrite_boots) do
+                prime.main_boots = nil
+                for i = 0, 5 do
+                    local item = prime:GetItemInSlot(i)
+                    -- if a pair of boots is found, do nothing
+                    if item ~= nil and item:GetAbilityName() == boots_name then
+                        -- print(item:GetAbilityName().." / "..boots_name)
+                        prime.main_boots = item
+                        rewrite_found = true
+                        break
+                    end
+                end
+                if rewrite_found == true then
+                    break
+                end
+            end
         end
+
+        local found_boots = caster:GetCloneSource().main_boots
+        -- Pair of boots found, do something
+        if rewrite_found == true then
+--			print("Boots found in main meepo:", found_boots:GetAbilityName())
+            if not caster:HasItemInInventory(found_boots:GetAbilityName()) then
+                local cloned_boots = caster:AddItemByName(found_boots:GetAbilityName())
+                if cloned_boots and caster:HasItemInInventory(found_boots:GetAbilityName()) and cloned_boots:GetItemSlot() ~= found_boots:GetItemSlot() then
+                    caster:SwapItems(cloned_boots:GetItemSlot(), found_boots:GetItemSlot())
+                end
+            end
+        else
+            for _, boots_name in pairs(rewrite_boots) do
+                for slot = 0, 8 do
+                    local item = caster:GetItemInSlot(slot)
+                    if item and item:GetName() == boots_name then
+                        caster:RemoveItem(item)
+                        break
+                    end
+                end
+            end
+        end
+        caster:CalculateStatBonus(true)
     end
 end
 
