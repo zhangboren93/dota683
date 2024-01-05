eaf6c0050145065f004a6655230268881219897f
Boren Zhang <bobo1993324@126.com>
2024-01-05 13:56:17 +0800

    Brewmaster drunken haze soundeffect.

--- scripts/vscripts/ai/ability_execute_tracker.lua
+++ scripts/vscripts/ai/ability_execute_tracker.lua
@@ -1,11 +1,11 @@
 function handleAbilityExecuted(event)
 	if not IsServer() then return end
 	local caster = event.caster
-	caster.lastAbilityCastTime = GameTime()
+	caster.lastAbilityCastTime = GameRules:GetGameTime()
 end
 
 function handleAttack(event)
 	if not IsServer() then return end
 	local attacker = event.attacker
-	attacker.lastAttackTime = GameTime()
+	attacker.lastAttackTime = GameRules:GetGameTime()
 end
