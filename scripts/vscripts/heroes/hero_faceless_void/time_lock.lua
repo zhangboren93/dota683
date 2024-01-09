036f1be6f3204d12ff82368dd61dbc7660638cc0
Boren Zhang <bobo1993324@126.com>
2024-01-09 13:35:53 +0800

    Void ability value tune.

--- scripts/vscripts/heroes/hero_faceless_void/time_lock.lua
+++ scripts/vscripts/heroes/hero_faceless_void/time_lock.lua
@@ -5,3 +5,27 @@ function handleAttackStart(event)
 		event.ability:ApplyDataDrivenModifier(attacker, attacker, event.ModifierName, {})
 	end
 end
+
+function handleAttackLanded(event)
+	local target = event.target
+	local attacker = event.attacker
+	local ability = event.ability
+	local chance = ability:GetSpecialValueFor("chance_pct")
+	local bonus_damage = ability:GetSpecialValueFor("bonus_damage")
+	local i = RandomInt(1, 100)
+	if i <= chance then
+		target:EmitSound("Hero_FacelessVoid.TimeLockImpact")
+		ability:ApplyDataDrivenModifier(attacker, target, "modifier_time_lock_stun_datadriven", { duration = 1 })
+		--double damage if in chronosphere
+		if target:HasModifier("modifier_faceless_void_chronosphere_freeze") then
+			bonus_damage = bonus_damage * 2
+		end
+		ApplyDamage({
+			attacker = attacker,
+			victim = target,
+			damage = bonus_damage,
+			damage_type = DAMAGE_TYPE_MAGICAL,
+			ability = ability
+		})
+	end
+end
