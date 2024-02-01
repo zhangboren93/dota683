e7c880ce77971e2bd063eddff2cd30001598e615
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-02-01 10:13:58 +0800

    Add rune denied combat event.

--- scripts/vscripts/units/aegis_attacked.lua
+++ scripts/vscripts/units/aegis_attacked.lua
@@ -4,4 +4,22 @@ function handleAttack(event)
 	if target:GetClassname() == "dota_item_drop" and target:GetModelName() == "models/props_gameplay/aegis.vmdl" then
 		CustomGameEventManager:Send_ServerToAllClients("aegis_destroyed", {kpid = attacker:GetPlayerOwnerID()})
 	end
+	if target:GetClassname() == "dota_item_rune" then
+		local rune = DOTA_RUNE_INVALID
+		if target:GetModelName() == "models/props_gameplay/rune_doubledamage01.vmdl" then
+			rune = DOTA_RUNE_DOUBLEDAMAGE
+		elseif target:GetModelName() == "models/props_gameplay/rune_haste01.vmdl" then
+			rune = DOTA_RUNE_HASTE
+		elseif target:GetModelName() == "models/props_gameplay/rune_illusion01.vmdl" then
+			rune = DOTA_RUNE_ILLUSION
+		elseif target:GetModelName() == "models/props_gameplay/rune_invisibility01.vmdl" then
+			rune = DOTA_RUNE_INVISIBILITY
+		elseif target:GetModelName() == "models/props_gameplay/rune_regeneration01.vmdl" then
+			rune = DOTA_RUNE_REGENERATION
+		elseif target:GetModelName() == "models/props_gameplay/rune_goldxp.vmdl" then
+			rune = DOTA_RUNE_BOUNTY
+		end
+		CustomGameEventManager:Send_ServerToTeam(attacker:GetTeam(), "player_rune_denied", {
+			pid = attacker:GetPlayerOwnerID(), rune_type = rune })
+	end
 end
