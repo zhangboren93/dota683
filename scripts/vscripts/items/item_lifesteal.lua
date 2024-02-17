28d2e438eedce94b1ca85985d0be39e7f1b2c501
Boren Zhang <bobo1993324@126.com>
2024-02-17 13:48:50 +0800

    Rewrite item lifesteal orb.

--- scripts/vscripts/items/item_lifesteal.lua
+++ scripts/vscripts/items/item_lifesteal.lua
@@ -2,6 +2,18 @@ function attack_landed(event)
     if not event.target:IsIllusion() 
 		and not event.target:IsBuilding() 
 		and event.target:GetTeam() ~= event.caster:GetTeam() then
-        event.ability:ApplyDataDrivenModifier(event.caster, event.caster, "modifier_item_lifesteal_datadriven", { duration = 0.03 })
+		event.caster:FindAbilityByName("hero_intrinstic_mechanism_datadriven"):
+					 ApplyDataDrivenModifier(event.caster, event.caster, "modifier_item_lifesteal_datadriven", { duration = 0.03 })
     end
+end
+
+item_lifesteal_datadriven = class({})
+
+function item_lifesteal_datadriven:GetIntrinsicModifierName()
+	return "modifier_generic_orb_effect_item_lua"
+end
+
+function item_lifesteal_datadriven:OnOrbImpact(event)
+	event.caster = self:GetCaster()
+	attack_landed(event)
 end
---
+++ scripts/vscripts/libraries/modifiers/modifier_generic_orb_item.lua
@@ -0,0 +1,67 @@
+-- Generic Orb Effect Library created by Elfansoer (with modifications by AltiV)
+-- See the reference at https://github.com/Elfansoer/dota-2-lua-abilities/blob/c3bfb93a32e8257f861a5e32e4d25231185a6ba4/scripts/vscripts/lua_abilities/generic/modifier_generic_orb_effect_item_lua.lua
+
+-- There's some weird bug that lets you bypass target flags if you level up the ability mid-attack -_-
+
+modifier_generic_orb_effect_item_lua = class({})
+
+--------------------------------------------------------------------------------
+-- Classifications
+function modifier_generic_orb_effect_item_lua:IsHidden()
+	return true
+end
+
+function modifier_generic_orb_effect_item_lua:IsDebuff()
+	return false
+end
+
+function modifier_generic_orb_effect_item_lua:IsPurgable()
+	return false
+end
+
+function modifier_generic_orb_effect_item_lua:RemoveOnDeath()
+	return false
+end
+
+--------------------------------------------------------------------------------
+-- Initializations
+function modifier_generic_orb_effect_item_lua:OnCreated( kv )
+	-- generate data
+	self.ability = self:GetAbility()
+	self.cast = false
+	self.records = {}
+	
+	-- Test variable for proper projectile effect display
+	self.target = nil
+end
+
+--------------------------------------------------------------------------------
+-- Modifier Effects
+function modifier_generic_orb_effect_item_lua:DeclareFunctions()
+	return {
+		MODIFIER_EVENT_ON_ATTACK,
+		MODIFIER_PROPERTY_PROCATTACK_FEEDBACK
+	}
+end
+
+function modifier_generic_orb_effect_item_lua:OnAttack( params )
+	if params.attacker~=self:GetParent() then return end
+
+	-- record the attack
+	self.records[params.record] = true
+
+	self.cast = false
+end
+
+function modifier_generic_orb_effect_item_lua:GetModifierProcAttack_Feedback( params )
+	if params.attacker ~= self:GetParent() then return end
+
+	-- don't proc if attacker has hero orb
+	local hero_orb_modifier = params.attacker:FindModifierByName("modifier_generic_orb_effect_lua")
+	if hero_orb_modifier ~= nil and hero_orb_modifier.records[params.record] then return end
+
+	if self.records[params.record] then
+		-- apply the effect
+		if self.ability.OnOrbImpact then self.ability:OnOrbImpact( params ) end
+	end
+end
