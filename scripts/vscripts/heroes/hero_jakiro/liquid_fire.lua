0a7532f775792d76090b47c1110783402add88cf
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-02-19 22:35:57 +0800

    Rewrite jakiro liquid fire.

---
+++ scripts/vscripts/heroes/hero_jakiro/liquid_fire.lua
@@ -0,0 +1,61 @@
+-- Created by Elfansoer
+--[[
+Ability checklist (erase if done/checked):
+- Scepter Upgrade
+- Break behavior
+- Linken/Reflect behavior
+- Spell Immune/Invulnerable/Invisible behavior
+- Illusion behavior
+- Stolen behavior
+]]
+--------------------------------------------------------------------------------
+jakiro_liquid_fire_lua = class({ 
+	GetIntrinsicModifierName = function(self) return "modifier_generic_orb_effect_lua" end,
+	GetProjectileName		 = function(self) return "particles/units/heroes/hero_jakiro/jakiro_base_attack_fire.vpcf" end
+})
+
+function jakiro_liquid_fire_lua:OnOrbFire( params )
+	self:GetCaster():EmitSound("Hero_Jakiro.LiquidFire")
+end
+
+function jakiro_liquid_fire_lua:OnOrbImpact( params )
+	local caster = self:GetCaster()
+
+	-- get data
+	local duration = self:GetDuration()
+	local radius = self:GetSpecialValueFor("radius")
+
+	-- find enemy in radius
+	local enemies = FindUnitsInRadius(
+		caster:GetTeamNumber(),
+		params.target:GetOrigin(),
+		nil,
+		radius,
+		DOTA_UNIT_TARGET_TEAM_ENEMY,
+		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_BUILDING,
+		0,
+		0,
+		false
+	)
+
+	for _,enemy in pairs(enemies) do
+		enemy:AddNewModifier(caster, self, "modifier_jakiro_liquid_fire_burn", { duration = duration })
+	end
+
+	-- play effects
+	self:PlayEffects( params.target, radius )
+end
+
+function jakiro_liquid_fire_lua:PlayEffects( target, radius )
+	-- Get Resources
+	local particle_cast = "particles/units/heroes/hero_jakiro/jakiro_liquid_fire_explosion.vpcf"
+	local sound_cast = "Hero_Jakiro.LiquidFire"
+
+	-- Create Particle
+	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, target )
+	ParticleManager:SetParticleControl( effect_cast, 1, Vector( radius, radius, radius ) )
+	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+	-- Create Sound
+	EmitSoundOn( sound_cast, self:GetCaster() )
+end
