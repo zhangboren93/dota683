ae4fa32a1731da1d089f1779e456f0d9b732c884
Boren Zhang <bobo1993324@126.com>
2024-03-30 13:06:47 +0800

    Remove maelstrom first strike delay.

--- scripts/vscripts/items/item_maelstrom.lua
+++ scripts/vscripts/items/item_maelstrom.lua
@@ -18,39 +18,37 @@ function modifier_item_maelstrom_datadriven_on_orb_impact(event)
 	end
 	caster:RemoveModifierByName("modifier_maelstrom_trigger_no_miss")
 	
-	caster:SetThink(function()
-		local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
-		ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))   
-		ParticleManager:SetParticleControl(particle,1,Vector(new_target:GetAbsOrigin().x,new_target:GetAbsOrigin().y,new_target:GetAbsOrigin().z + new_target:GetBoundingMaxs().z ))
-    	new_target:EmitSound("Item.Maelstrom.Chain_Lightning")
-		ApplyDamage({ victim = new_target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
-    	count = count - 1
+	local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
+	ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))   
+	ParticleManager:SetParticleControl(particle,1,Vector(new_target:GetAbsOrigin().x,new_target:GetAbsOrigin().y,new_target:GetAbsOrigin().z + new_target:GetBoundingMaxs().z ))
+    new_target:EmitSound("Item.Maelstrom.Chain_Lightning")
+	ApplyDamage({ victim = new_target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
+    count = count - 1
 
-    	caster:SetThink(function()
-    	    target = new_target
-    	    new_target = nil
-			local units = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, chain_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false)
-    	    for i=1,#units do
-    	        if not units[i]:IsMagicImmune() and victims[units[i]:GetEntityIndex()] == nil then
-    	            new_target = units[i]
-    	            break
-    	        end
-    	    end
-    	    if new_target == nil then
-    	        return
-    	    end
-    	    victims[new_target:GetEntityIndex()] = true
-    		local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
-    		ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))   
-    		ParticleManager:SetParticleControl(particle,1,Vector(new_target:GetAbsOrigin().x,new_target:GetAbsOrigin().y,new_target:GetAbsOrigin().z + new_target:GetBoundingMaxs().z ))
-    	    new_target:EmitSound("Item.Maelstrom.Chain_Lightning.Jump")
-    		ApplyDamage({ victim = new_target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
-    	    count = count - 1
-    	    if count > 0 then
-    	        return chain_delay
-    	    end
-    	end, "finds another target", chain_delay)
-	end, "delay triggering chain", chain_delay)
+    caster:SetThink(function()
+        target = new_target
+        new_target = nil
+		local units = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, chain_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false)
+        for i=1,#units do
+            if not units[i]:IsMagicImmune() and victims[units[i]:GetEntityIndex()] == nil then
+                new_target = units[i]
+                break
+            end
+        end
+        if new_target == nil then
+            return
+        end
+        victims[new_target:GetEntityIndex()] = true
+    	local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
+    	ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))   
+    	ParticleManager:SetParticleControl(particle,1,Vector(new_target:GetAbsOrigin().x,new_target:GetAbsOrigin().y,new_target:GetAbsOrigin().z + new_target:GetBoundingMaxs().z ))
+        new_target:EmitSound("Item.Maelstrom.Chain_Lightning.Jump")
+    	ApplyDamage({ victim = new_target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
+        count = count - 1
+        if count > 0 then
+            return chain_delay
+        end
+    end, "finds another target", chain_delay)
 end
 
 function handleOrbFire(event)
