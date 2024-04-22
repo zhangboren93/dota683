ab3e8c780348b00f3893e7b72cacfb78414c2652
Boren Zhang <bobo1993324@126.com>
2024-04-22 13:15:16 +0800

    Fix player dropped team xp calculation.

--- scripts/vscripts/kill_bonus.lua
+++ scripts/vscripts/kill_bonus.lua
@@ -373,12 +373,15 @@ function GetTeamTotalXP(victim_team)
 	local victim_team_total_gold = 0
 	for i=1,PlayerResource:GetPlayerCountForTeam(victim_team) do
 		local playerId = PlayerResource:GetNthPlayerIDOnTeam(victim_team, i)
-		local hero = PlayerResource:GetPlayer(playerId):GetAssignedHero()
-		if hero == nil then
-			GameRules:SendCustomMessage("[WARN] no assigned hero for player " .. playerId, -1, -1)
-		else
-			victim_team_total_gold = victim_team_total_gold + PlayerResource:GetPlayer(playerId):GetAssignedHero():GetCurrentXP()
-			--print(victim_team .. " " .. playerId .. " " .. victim_team_total_gold)
+		local player = PlayerResource:GetPlayer(playerId)
+		if player ~= nil then
+			local hero = player:GetAssignedHero()
+			if hero == nil then
+				GameRules:SendCustomMessage("[WARN] no assigned hero for player " .. playerId, -1, -1)
+			else
+				victim_team_total_gold = victim_team_total_gold + hero:GetCurrentXP()
+				--print(victim_team .. " " .. playerId .. " " .. victim_team_total_gold)
+			end
 		end
 	end
 	return victim_team_total_gold
