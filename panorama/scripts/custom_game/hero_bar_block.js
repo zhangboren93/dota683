59eb4b0719d6e2c0f525f7c8c69da406a72464f5
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-05-21 11:34:04 +0800

    Hero bar block lose effect when alt is pressed.

--- panorama/scripts/custom_game/hero_bar_block.js
+++ panorama/scripts/custom_game/hero_bar_block.js
@@ -1,5 +1,14 @@
-(function() {
+
+function TrackAltPressed(panel)
+{
+	panel.SetHasClass("AltPressed", IsDotaAltPressed());
+	$.Schedule(0.0, () => TrackAltPressed(panel));
+}
+
+(function ()
+{
 	if (Players.GetTeam(Players.GetLocalPlayer()) === DOTATeam_t.DOTA_TEAM_GOODGUYS) {
 		$("#hero-bar-block-buttons").AddClass("dire-block");
 	}
+	TrackAltPressed($("#hero-bar-block-buttons"));
 })();
