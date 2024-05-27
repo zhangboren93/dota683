40613e9dd5369cdff20b1288d1cf8faf36838c19
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-05-27 08:44:31 +0800

    Silencer use innate ability.

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1121,7 +1121,6 @@ function HandleNpcSpawned(self, entityIndex, is_respawn)
 		elseif entity:GetName() == "npc_dota_hero_silencer" then
 			local ability = entity:FindAbilityByName("silencer_global_silence_aghs_datadriven")
 			ability:SetLevel(1)
-			entity:AddNewModifier(entity, ability, "modifier_silencer_glaives_of_wisdom", {})
 		elseif entity:GetName() == "npc_dota_hero_invoker" then
 			entity:SetThink(function()
 				entity:FindAbilityByName("invoker_invoke"):SetLevel(0)
