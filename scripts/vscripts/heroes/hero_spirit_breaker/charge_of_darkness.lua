c8632764f329fc89262fa7f83c8822f997071e73
Boren Zhang <bobo1993324@126.com>
2024-06-25 07:22:21 +0800

    charge of darkness add gesture.

--- scripts/vscripts/heroes/hero_spirit_breaker/charge_of_darkness.lua
+++ scripts/vscripts/heroes/hero_spirit_breaker/charge_of_darkness.lua
@@ -1,12 +1,13 @@
 require("../../items/item_magic_stick")
 function handleSpellStart(event)
-    local target = event.target
-    local ability = event.ability
+	local target = event.target
+	local ability = event.ability
 	ProcsMagicStick(event)
-    if target:TriggerSpellAbsorb(ability) then
-        return
-    end
-    local caster = event.caster
-    caster:EmitSound("Hero_Spirit_Breaker.ChargeOfDarkness")
-    caster:AddNewModifier(caster, ability, "modifier_spirit_breaker_charge_of_darkness_lua", { target = target:GetEntityIndex() })
+	if target:TriggerSpellAbsorb(ability) then
+		return
+	end
+	local caster = event.caster
+	caster:EmitSound("Hero_Spirit_Breaker.ChargeOfDarkness")
+	caster:AddNewModifier(caster, ability, "modifier_spirit_breaker_charge_of_darkness_lua", { target = target:GetEntityIndex() })
+	caster:StartGesture(ACT_RUN)
 end
