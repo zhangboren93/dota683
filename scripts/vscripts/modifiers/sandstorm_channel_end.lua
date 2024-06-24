dd03080f067d21a2f88fda936ced9cfad0e37142
Boren Zhang <bobo1993324@126.com>
2024-06-24 13:24:49 +0800

    Fix sandking storm+burrow+storm will cancel storm problem.

--- scripts/vscripts/modifiers/sandstorm_channel_end.lua
+++ scripts/vscripts/modifiers/sandstorm_channel_end.lua
@@ -1,28 +1,32 @@
 if modifier_sandstorm_channel_end == nil then
-    modifier_sandstorm_channel_end = class({})
+	modifier_sandstorm_channel_end = class({})
 end
 
 function modifier_sandstorm_channel_end:GetAttributes()
-    return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+	return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
 end
 
 function modifier_sandstorm_channel_end:DeclareFunctions()
-    local funcs = {
-        MODIFIER_EVENT_ON_ABILITY_END_CHANNEL
-    }
-    return funcs
+	local funcs = {
+		MODIFIER_EVENT_ON_ABILITY_END_CHANNEL
+	}
+	return funcs
 end
 
 function modifier_sandstorm_channel_end:IsHidden()
-    return true
+	return true
 end
 
 function modifier_sandstorm_channel_end:OnAbilityEndChannel(event)
-    local unit = event.unit
-    local ability = event.ability
-    if ability:GetName() == "sandking_sand_storm" then
-        unit:SetThink(function()
-            unit:RemoveModifierByName("modifier_sandking_sand_storm")
-        end, "exit sand storm", ability:GetSpecialValueFor("fade_delay"))
-    end
+	local unit = event.unit
+	local ability = event.ability
+	local fade_delay = ability:GetSpecialValueFor("fade_delay")
+	if ability:GetName() == "sandking_sand_storm" then
+		unit:SetThink(function()
+			-- check if sandking has casted another sandstorm
+			if ability:GetCooldown(-1) - ability:GetCooldownTimeRemaining() > fade_delay then
+				unit:RemoveModifierByName("modifier_sandking_sand_storm")
+			end
+		end, "exit sand storm", fade_delay)
+	end
 end
