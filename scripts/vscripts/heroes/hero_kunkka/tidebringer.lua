1766cc4d723b89e3fa797aa3b7d415d5d8ab6ab4
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-07-02 21:36:58 +0800

    Tidebringer deprecated part remove.

--- scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
+++ scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
@@ -1,26 +1,3 @@
---[[
-	Author: kritth
-	Date: 09.01.2015
-	Reset cooldown after attack is landed
-	This function have been deprecated.
-]]
-function tidebringer_set_cooldown( keys )
-	-- Variables
-	local caster = keys.caster
-	local ability = keys.ability
-	local cooldown = ability:GetCooldown( ability:GetLevel() - 1)
-	local modifierName = "modifier_tidebringer_splash_datadriven"
-	
-	-- Remove cooldown
-	caster:RemoveModifierByName( modifierName )
-	ability:StartCooldown( cooldown )
-	caster:SetThink(function()
-			ability:ApplyDataDrivenModifier( caster, caster, modifierName, {} )
-			return nil
-		end, "tidebringer cooldown", cooldown)
-
-end
-
 function handleIntervalThink(event)
 	local caster = event.caster
 	local ability = event.ability
