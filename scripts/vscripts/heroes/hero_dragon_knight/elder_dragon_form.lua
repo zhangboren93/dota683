7fa62df74b94d44406575e12ce734bea0aaf7c34
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-07-02 09:19:56 +0800

    Remove deprecated modifiers in hook.

--- scripts/vscripts/heroes/hero_dragon_knight/elder_dragon_form.lua
+++ scripts/vscripts/heroes/hero_dragon_knight/elder_dragon_form.lua
@@ -269,22 +269,3 @@ end
 function modifier_ability_elder_dragon_form_corrosive:GetEffectAttachType()
 	return PATTACH_ABSORIGIN_FOLLOW
 end
-
--- Deprecated
-
-function handleAttackLanded(event)
-	if event.caster:HasModifier("modifier_dragon_knight_corrosive_breath") then
-		event.ability:ApplyDataDrivenModifier(event.caster, event.target, 
-			"modifier_dragon_knight_corrosive_breath_dot_datadriven", 
-			{ duration = 5 })
-	end
-end
-
-function handleIntervalThink(event)
-	ApplyDamage({
-		victim = event.target, 
-		attacker = event.caster, 
-		damage = 20, 
-		damage_type = DAMAGE_TYPE_MAGICAL, 
-		damage_flags = DOTA_DAMAGE_FLAG_NON_LETHAL})
-end
