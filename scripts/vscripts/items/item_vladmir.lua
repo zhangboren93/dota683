065ed384f3fd90393b705b4071b696b1f68d3f80
Boren Zhang <bobo1993324@126.com>
2024-07-08 13:55:21 +0800

    Fix vladmir aura works on all tidebringer targets.

--- scripts/vscripts/items/item_vladmir.lua
+++ scripts/vscripts/items/item_vladmir.lua
@@ -3,6 +3,10 @@ function VladmirAuraApply(event)
 	local target = event.target
 	local ability = event.ability
 	if not target:IsIllusion() and not target:IsBuilding() and target:GetTeam() ~= attacker:GetTeam() then
-		ability:ApplyDataDrivenModifier(attacker, attacker, "modifier_item_vladmir_datadriven_lifesteal", {duration = 0.03})
+		local damage = event.Damage
+		local lifesteal_percent = ability:GetSpecialValueFor("lifesteal_aura_percent")
+		attacker:Heal(damage * lifesteal_percent / 100, ability)
+		local particleId = ParticleManager:CreateParticle("particles/generic_gameplay/generic_lifesteal.vpcf", PATTACH_ABSORIGIN_FOLLOW, attacker)
+		ParticleManager:ReleaseParticleIndex(particleId)
 	end
 end
