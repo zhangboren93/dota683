10b149c44e0a36f5626d613df0e265ea7794f5d3
Boren Zhang <bobo1993324@126.com>
2024-07-11 08:23:52 +0800

    Techies mine invisble effect & stasis sound.

--- scripts/vscripts/heroes/hero_techies/modifier_techies_stasis_trap_explode_sound.lua
+++ scripts/vscripts/heroes/hero_techies/modifier_techies_stasis_trap_explode_sound.lua
@@ -2,6 +2,6 @@ modifier_techies_stasis_trap_explode_sound_lua = class({
 	OnDestroy = function(self) 
 		if not IsServer() then return end
 		--TODO use real stasis trap detonate sound
-		EmitSoundOnLocationWithCaster(self:GetParent():GetAbsOrigin(), "Hero_Techies.StickyBomb.Detonate", self:GetCaster())
+		EmitSoundOnLocationWithCaster(self:GetParent():GetAbsOrigin(), "Hero_Techies.ReactiveTazer.Detonate", self:GetCaster())
 	end
 })
