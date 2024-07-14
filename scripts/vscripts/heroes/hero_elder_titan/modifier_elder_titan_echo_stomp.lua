d830184faad81ce884a2b2d46ff7259b02058fa7
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-07-14 10:13:12 +0800

    Elder titan echo stomp bug fix.

--- scripts/vscripts/heroes/hero_elder_titan/modifier_elder_titan_echo_stomp.lua
+++ scripts/vscripts/heroes/hero_elder_titan/modifier_elder_titan_echo_stomp.lua
@@ -14,6 +14,7 @@ end
 
 function modifier_elder_titan_echo_stomp_lua:OnTakeDamage(event)
 	-- do nothing in the initial stun duration
+	if event.unit ~= self:GetParent() then return end
 	local initial_stun_duration = self:GetAbility():GetSpecialValueFor("initial_stun_duration")
 	local sleep_duration = self:GetAbility():GetSpecialValueFor("sleep_duration")
 	if self:GetRemainingTime() > sleep_duration - initial_stun_duration then
