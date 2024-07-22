88e3eceaed5b6d2c6e0ad72d43718441c29a2123
U-MS-MVTGEZKMUKTG\Administrator <bobo1993324@126.com>
2024-07-22 22:55:20 +0800

    Simplify glyph flipped css.

--- panorama/scripts/custom_game/glyph.js
+++ panorama/scripts/custom_game/glyph.js
@@ -14,20 +14,20 @@ function handleTeamGlyphCDTick(event) {
 
 GameEvents.Subscribe("team_glyph_cooldown_tick", handleTeamGlyphCDTick)
 
-function pollFlippedHudStatus() {
-	let panel = $.GetContextPanel();
-	let rootHud = panel.GetParent().GetParent().GetParent();
-	if (rootHud.BHasClass("HUDFlipped")) {
-		if (!panel.BHasClass("RootHudFlipped")) {
-			panel.AddClass("RootHudFlipped");
-		}
-	} else {
-		if (panel.BHasClass("RootHudFlipped")) {
-			panel.RemoveClass("RootHudFlipped");
-		}
-	}
-	$.Schedule(1, pollFlippedHudStatus);
-}
-(function(){
-	$.Schedule(1, pollFlippedHudStatus)
-})()
+//function pollFlippedHudStatus() {
+//	let panel = $.GetContextPanel();
+//	let rootHud = panel.GetParent().GetParent().GetParent();
+//	if (rootHud.BHasClass("HUDFlipped")) {
+//		if (!panel.BHasClass("RootHudFlipped")) {
+//			panel.AddClass("RootHudFlipped");
+//		}
+//	} else {
+//		if (panel.BHasClass("RootHudFlipped")) {
+//			panel.RemoveClass("RootHudFlipped");
+//		}
+//	}
+//	$.Schedule(1, pollFlippedHudStatus);
+//}
+//(function(){
+//	$.Schedule(1, pollFlippedHudStatus)
+//})()
