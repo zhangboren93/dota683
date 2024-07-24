58a0eeb2f6ea9ae83380d1fc5ac70786e10a8299
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-07-24 20:11:31 +0800

    Faceless void chronosphere movespeed bug fix.

--- scripts/vscripts/modifiers/attribute_regen.lua
+++ scripts/vscripts/modifiers/attribute_regen.lua
@@ -42,6 +42,8 @@ function modifier_attribute_regen_adjust:GetModifierMoveSpeed_Limit()
         return self:GetParent():FindAbilityByName("spirit_breaker_charge_of_darkness"):GetSpecialValueFor("movement_speed")
     elseif parent:HasModifier("modifier_lycan_shapeshift_speed") then
         return 650
+    elseif parent:HasModifier("modifier_faceless_void_chronosphere_speed") then
+        return 1000
     end
     return 522
 end
