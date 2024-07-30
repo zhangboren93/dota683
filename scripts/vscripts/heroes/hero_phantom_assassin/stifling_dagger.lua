f0d7a00704521feec070564edd535defd9cbe84b
Boren Zhang <bobo1993324@126.com>
2024-07-30 21:16:46 +0800

    Stifling dagger crit effect.

--- scripts/vscripts/heroes/hero_phantom_assassin/stifling_dagger.lua
+++ scripts/vscripts/heroes/hero_phantom_assassin/stifling_dagger.lua
@@ -12,6 +12,12 @@ function handleProjectileHitUnit(event)
 		local crit_bonus = coup:GetSpecialValueFor("crit_bonus")
 		if RandomInt(1, 100) <= crit_chance then
 			damage = damage * crit_bonus / 100
+			local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_phantom_assassin/phantom_assassin_crit_impact.vpcf",
+				PATTACH_CUSTOMORIGIN_FOLLOW, target)
+			ParticleManager:SetParticleControlEnt(particle, 0, target, PATTACH_CUSTOMORIGIN_FOLLOW , "attach_hitloc", Vector(0, 0, 0), false)
+			ParticleManager:SetParticleControlTransformForward(particle, 1, target:GetAbsOrigin(), -caster:GetForwardVector())
+			ParticleManager:ReleaseParticleIndex(particle)
+			target:EmitSound("Hero_PhantomAssassin.CoupDeGrace")
 		end
 	end
 	ApplyDamage({
