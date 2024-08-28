813baf7a544b830825a11c82ad3b68c71dfe35d1
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-08-28 08:34:20 +0800

    Infest bug fix.

--- scripts/vscripts/heroes/hero_life_stealer/infest.lua
+++ scripts/vscripts/heroes/hero_life_stealer/infest.lua
@@ -17,7 +17,6 @@ function infest_check_valid( keys )
     print(target:GetUnitLabel())
     print(target:GetUnitName())
 
-	ProcsMagicStick(keys)
     --check for validity. theres a lot of exceptions, and i'd like a better way to do this.
     --unsure of the formatting as well as it's a long list.
     local enemyexceptionlist = {"spirit_bear", "visage_familiars"}
