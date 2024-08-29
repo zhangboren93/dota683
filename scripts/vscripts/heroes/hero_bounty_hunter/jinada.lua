da811e07828285e37c32f86f9e90c5ecd979b593
Boren Zhang <bobo1993324@126.com>
2024-08-29 14:11:45 +0800

    Add jinada ready effect

--- scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
+++ scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
@@ -20,6 +20,12 @@ function Jinada( keys )
 	if caster:IsRealHero() and not target:IsBuilding() then
 		ability:ApplyDataDrivenModifier(caster, target, "modifier_jinada_slow_datadriven", {})
 	end
+	if caster.jinada_ready_pid ~= nil then
+		ParticleManager:DestroyParticle(caster.jinada_ready_pid, false)
+		ParticleManager:DestroyParticle(caster.jinada_ready_pid2, false)
+		caster.jinada_ready_pid = nil
+		caster.jinada_ready_pid2 = nil
+	end
 end
 
 function handleAttackStart(event)
@@ -39,5 +45,16 @@ function handleAttackStart(event)
 	else
 		attacker:RemoveModifierByName("modifier_jinada_datadriven")
 		attacker:RemoveModifierByName("modifier_jinada_breaked_datadriven")
+	end
+end
+
+function handleIntervalThink(event)
+	local caster = event.caster
+	local ability = event.ability
+	if ability:IsCooldownReady() and caster.jinada_ready_pid == nil then
+		caster.jinada_ready_pid = ParticleManager:CreateParticle("particles/units/heroes/hero_bounty_hunter/bounty_hunter_hand_r.vpcf", PATTACH_POINT_FOLLOW, caster)
+		ParticleManager:SetParticleControlEnt(caster.jinada_ready_pid, 0, caster, PATTACH_POINT_FOLLOW, "attach_weapon1", Vector(0, 0, 0), false)
+		caster.jinada_ready_pid2 = ParticleManager:CreateParticle("particles/units/heroes/hero_bounty_hunter/bounty_hunter_hand_l.vpcf", PATTACH_POINT_FOLLOW, caster)
+		ParticleManager:SetParticleControlEnt(caster.jinada_ready_pid2, 0, caster, PATTACH_POINT_FOLLOW, "attach_weapon2", Vector(0, 0, 0), false)
 	end
 end
