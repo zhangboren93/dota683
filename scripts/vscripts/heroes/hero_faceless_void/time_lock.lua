804c19182c7e04707dd19445c63648c0084ea7ae
Boren Zhang <bobo1993324@gmail.com>
2024-09-29 13:25:04 +0800

    Faceless void passive won't work on illusions.

--- scripts/vscripts/heroes/hero_faceless_void/time_lock.lua
+++ scripts/vscripts/heroes/hero_faceless_void/time_lock.lua
@@ -1,7 +1,7 @@
 function handleAttackStart(event)
 	local target = event.target
 	local attacker = event.attacker
-	if target:GetTeam() ~= attacker:GetTeam() and not target:IsBuilding() and not attacker:PassivesDisabled() then
+	if target:GetTeam() ~= attacker:GetTeam() and not target:IsBuilding() and not attacker:PassivesDisabled() and not attacker:IsIllusion() then
 		event.ability:ApplyDataDrivenModifier(attacker, attacker, event.ModifierName, {})
 	end
 end
