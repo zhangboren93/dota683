7ec6846007c14f7a132d9424932c9a4279521c56
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-09-29 13:09:47 +0800

    panorama script bug fix.

--- panorama/scripts/custom_game/combat_events.js
+++ panorama/scripts/custom_game/combat_events.js
@@ -118,7 +118,7 @@ function OnTeamBountyBuildingDestroyed(event) {
 	$.Msg("OnTeamBountyBuildingDestroyed " + event.bname + " " + BUILDING_NAME_2_TYPE_ID[event.bname]);
 	let buildingTeam = getBuildingTeam(event.bname);
 	if(event.kpid == -1 || buildingTeam != Players.GetTeam(event.kpid)) {
-		let parentPanel = $.GetContextPanel()
+		let parentPanel = $.GetContextPanel();
 		let newChildPanel = $.CreatePanel( "Panel", parentPanel, "tbbd_" + event.kpid + "x" + event.bname + "g" + event.gold);
 		newChildPanel.BLoadLayout("file://{resources}/layout/custom_game/combat_event_building_destroyed.xml", false, false);
 		if (event.kpid == -1) {
