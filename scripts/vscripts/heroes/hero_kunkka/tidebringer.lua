45483adfc00a1009fef825ec3125c40221b24957
Boren Zhang <bobo1993324@gmail.com>
2024-09-30 22:07:32 +0800

    Adjust tidebringer trigger condition, only triggers on modified attacks.

--- scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
+++ scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
@@ -2,8 +2,20 @@ function handleIntervalThink(event)
 	local caster = event.caster
 	local ability = event.ability
 	if ability:IsCooldownReady() then
-		if not caster:HasModifier("modifier_tidebringer_cleave") then
+		if not caster:HasModifier("modifier_tidebringer_cleave") and not caster:IsAttacking() then
 			caster:AddNewModifier(caster, ability, "modifier_tidebringer_cleave", {})
 		end
 	end
+end
+
+function handleAttack(event)
+	local caster = event.caster
+	caster:SetThink(function()
+		local ability = event.ability
+		if ability:IsCooldownReady() then
+			if not caster:HasModifier("modifier_tidebringer_cleave") then
+				caster:AddNewModifier(caster, ability, "modifier_tidebringer_cleave", {})
+			end
+		end
+	end, "Adds tidebringer effect.", 0.1)
 end
