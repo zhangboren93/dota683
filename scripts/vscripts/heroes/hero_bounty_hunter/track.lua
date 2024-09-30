374155370b0c1b78c34cf8726a43ddf2e1ecaa93
Boren Zhang <bobo1993324@gmail.com>
2024-09-30 21:53:31 +0800

    Track effect visible to own team.

--- scripts/vscripts/heroes/hero_bounty_hunter/track.lua
+++ scripts/vscripts/heroes/hero_bounty_hunter/track.lua
@@ -7,10 +7,12 @@ function handleSpellStart(event)
 	local modifier = target:AddNewModifier(caster, ability, "modifier_bounty_hunter_track_lua", { duration = duration })
 	target:EmitSound("Hero_BountyHunter.Target")
 	if modifier.particleId == nil then
-		modifier.particleId = ParticleManager:CreateParticle("particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_trail.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+		modifier.particleId = ParticleManager:CreateParticleForTeam(
+			"particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_trail.vpcf", PATTACH_ABSORIGIN_FOLLOW, target, caster:GetTeam())
 		ParticleManager:SetParticleControlEnt(modifier.particleId, 0, target, PATTACH_ABSORIGIN_FOLLOW, "", Vector(0, 0, 0), false)
 		ParticleManager:SetParticleControlEnt(modifier.particleId, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", Vector(0, 0, 0), false)
 		target:AddNewModifier(caster, ability, "modifier_bounty_hunter_track_aura_lua", { duration = duration })
-		modifier.particleId2 = ParticleManager:CreateParticle("particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_shield.vpcf", PATTACH_OVERHEAD_FOLLOW, target)
+		modifier.particleId2 = ParticleManager:CreateParticleForTeam(
+			"particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_shield.vpcf", PATTACH_OVERHEAD_FOLLOW, target, caster:GetTeam())
 	end
 end
