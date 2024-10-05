650c52828caddb1a21bb76084e2e55cb82fd738e
U-Y007\Administrator <bobo1993324@126.com>
2024-10-06 03:05:29 +0800

    Track sound and effect only visible to bounties team

--- scripts/vscripts/heroes/hero_bounty_hunter/track.lua
+++ scripts/vscripts/heroes/hero_bounty_hunter/track.lua
@@ -5,7 +5,10 @@ function handleSpellStart(event)
 	local duration = ability:GetSpecialValueFor("duration")
 	if target:TriggerSpellAbsorb(ability) then return end
 	local modifier = target:AddNewModifier(caster, ability, "modifier_bounty_hunter_track_lua", { duration = duration })
-	target:EmitSound("Hero_BountyHunter.Target")
+	local team = caster:GetTeam()
+	for i=1,PlayerResource:GetPlayerCountForTeam(team) do
+		EmitSoundOnEntityForPlayer("Hero_BountyHunter.Target", target, PlayerResource:GetNthPlayerIDOnTeam(team, i))
+	end
 	if modifier.particleId == nil then
 		modifier.particleId = ParticleManager:CreateParticleForTeam(
 			"particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_trail.vpcf", PATTACH_ABSORIGIN_FOLLOW, target, caster:GetTeam())
@@ -15,4 +18,8 @@ function handleSpellStart(event)
 		modifier.particleId2 = ParticleManager:CreateParticleForTeam(
 			"particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_shield.vpcf", PATTACH_OVERHEAD_FOLLOW, target, caster:GetTeam())
 	end
+	local particleId = ParticleManager:CreateParticleForTeam(
+		"particles/units/heroes/hero_bounty_hunter/bounty_hunter_track_cast.vpcf", PATTACH_CUSTOMORIGIN, caster, caster:GetTeam())
+	ParticleManager:SetParticleControlEnt(particleId, 0, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetAbsOrigin(), false)
+	ParticleManager:SetParticleControlEnt(particleId, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), false)
 end
