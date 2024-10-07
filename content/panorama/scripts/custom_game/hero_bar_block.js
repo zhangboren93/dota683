a7a92a3a176418d464d54566dfc4bc81482e3cd4
Boren Zhang <bobo1993324@126.com>
2024-10-07 21:49:37 +0800

    Add back hero bar block.

--- content/panorama/scripts/custom_game/hero_bar_block.js
+++ content/panorama/scripts/custom_game/hero_bar_block.js
@@ -9,6 +9,10 @@ function TrackAltPressed(panel)
 {
 	if (Players.GetTeam(Players.GetLocalPlayer()) === DOTATeam_t.DOTA_TEAM_GOODGUYS) {
 		$("#hero-bar-block-buttons").AddClass("dire-block");
+		$("#hero-bar-block-buttons").visible = true
+	} else if (Players.GetTeam(Players.GetLocalPlayer()) === DOTATeam_t.DOTA_TEAM_BADGUYS) {
+		$("#hero-bar-block-buttons").AddClass("radiant-block");
+		$("#hero-bar-block-buttons").visible = true
 	}
 	TrackAltPressed($("#hero-bar-block-buttons"));
 })();
