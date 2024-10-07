265929beaed4058e5ba1265a506729003207205b
Boren Zhang <bobo1993324@126.com>
2024-10-07 23:38:46 +0800

    press the attack particle won't show in middle of the map.

--- scripts/vscripts/heroes/hero_legion_commander/press_the_attack.lua
+++ scripts/vscripts/heroes/hero_legion_commander/press_the_attack.lua
@@ -18,6 +18,18 @@ end
 function PtAStart(event)
 	local target = event.target
     local ability = event.ability
-    ability:ApplyDataDrivenModifier(event.caster, target, "modifier_legion_commander_press_the_attack_datadriven", {})
+    local modifier = ability:ApplyDataDrivenModifier(event.caster, target, "modifier_legion_commander_press_the_attack_datadriven", {})
+	if target.press_attack_particle_id == nil then
+		target.press_attack_particle_id = ParticleManager:CreateParticle("particles/units/heroes/hero_legion_commander/legion_commander_press.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+		ParticleManager:SetParticleControlEnt(target.press_attack_particle_id, 1, target, PATTACH_ABSORIGIN_FOLLOW, "attach_hitloc", Vector(0, 0, 0), false)
+	end
     target:Purge(false, true, false, true, true)
+end
+
+function handleDestroy(event)
+	local target = event.target
+	if target.press_attack_particle_id ~= nil then
+		ParticleManager:DestroyParticle(target.press_attack_particle_id, false)
+		target.press_attack_particle_id = nil
+	end
 end
