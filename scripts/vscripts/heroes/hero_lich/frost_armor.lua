8a66cd947aadce4bb021d55ca3717702026cc09f
Boren Zhang <bobo1993324@gmail.com>
2024-10-10 13:12:35 +0800

    remake lich frost armor.

--- scripts/vscripts/heroes/hero_lich/frost_armor.lua
+++ scripts/vscripts/heroes/hero_lich/frost_armor.lua
@@ -1,12 +1,81 @@
 function handleAbilityExecuted(event)
-	if event.event_ability:GetName() == "lich_frost_armor" or
-		event.event_ability:GetName() == "lich_frost_armor_2" then
-		event.target:EmitSound("n_creep_OgreMagi.FrostArmor")
-	elseif event.event_ability:GetName() == "lich_dark_ritual_datadriven" or
+	if event.event_ability:GetName() == "lich_dark_ritual_datadriven" or
 		event.event_ability:GetName() == "lich_dark_ritual_datadriven_2" then
 		event.caster:EmitSound("Hero_Lich.SinisterGaze.Cast")
 		event.caster:SetThink(function()
 			event.caster:StopSound("Hero_Lich.SinisterGaze.Cast")
 		end, "stop sound", 1)
 	end
+end
+
+--[[
+	Handles the AutoCast Logic
+	Author: Noya
+	Date: 18.01.2015.
+
+	Auto-Cast can interrupt current orders and forget the next queued command. Following queued commands are not forgotten
+	Cannot occur while channeling a spell.
+]]
+function FrostArmorAutocast( event )
+	local caster = event.caster
+	local target = event.target -- victim of the attack
+	local ability = event.ability
+
+	-- Name of the modifier to avoid casting the spell on targets that were already buffed
+	local modifier = "modifier_lich_frost_armor_datadriven"
+
+	-- Get if the ability is on autocast mode and cast the ability on the attacked target if it doesn't have the modifier
+	if ability:GetAutoCastState() then
+		if not IsChanneling( caster ) then
+			if not target:HasModifier(modifier) then
+				caster:CastAbilityOnTarget(target, ability, caster:GetPlayerOwnerID())
+			end
+		end	
+	end	
+end
+
+-- Auxiliar function that goes through every ability and item, checking for any ability being channelled
+function IsChanneling ( hero )
+	
+	for abilitySlot=0,15 do
+		local ability = hero:GetAbilityByIndex(abilitySlot)
+		if ability ~= nil and ability:IsChanneling() then 
+			return true
+		end
+	end
+
+	for itemSlot=0,5 do
+		local item = hero:GetItemInSlot(itemSlot)
+		if item ~= nil and item:IsChanneling() then
+			return true
+		end
+	end
+
+	return false
+end
+
+--[[
+	Author: Noya
+	Date: 18.1.2015.
+	Plays the lich_frost_armor particle and destroys it later
+]]
+function FrostArmorParticle( event )
+	local target = event.target
+	local location = target:GetAbsOrigin()
+	local particleName = "particles/units/heroes/hero_lich/lich_frost_armor.vpcf"
+
+	-- Particle. Need to wait one frame for the older particle to be destroyed
+	target:SetThink(function()
+		target.FrostArmorParticle = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, target)
+		ParticleManager:SetParticleControl(target.FrostArmorParticle, 0, target:GetAbsOrigin())
+		ParticleManager:SetParticleControl(target.FrostArmorParticle, 1, Vector(1,0,0))
+
+		ParticleManager:SetParticleControlEnt(target.FrostArmorParticle, 2, target, PATTACH_ABSORIGIN_FOLLOW, "attach_origin", target:GetAbsOrigin(), true)
+	end, "frost armor particle", 0.01)
+end
+
+-- Destroys the particle when the modifier is destroyed
+function EndFrostArmorParticle( event )
+	local target = event.target
+	ParticleManager:DestroyParticle(target.FrostArmorParticle,false)
 end
