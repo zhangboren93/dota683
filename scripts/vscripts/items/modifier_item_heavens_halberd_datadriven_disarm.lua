4da75b7fdef060d5f76ba0dca7b46a1d36d731e7
Boren Zhang <bobo1993324@gmail.com>
2024-10-16 18:30:49 +0800

    Halbert's disarm only dispellable by spell immunity.

--- scripts/vscripts/items/modifier_item_heavens_halberd_datadriven_disarm.lua
+++ scripts/vscripts/items/modifier_item_heavens_halberd_datadriven_disarm.lua
@@ -1,7 +1,7 @@
 modifier_item_heavens_halberd_datadriven_disarm = class({})
 function modifier_item_heavens_halberd_datadriven_disarm:CheckState()
 	return {
-		[MODIFIER_STATE_DISARMED] = true
+		[ MODIFIER_STATE_DISARMED ] = true
 	}
 end
 
@@ -10,7 +10,7 @@ function modifier_item_heavens_halberd_datadriven_disarm:IsPurgable()
 end
 
 function modifier_item_heavens_halberd_datadriven_disarm:IsPurgeException()
-	return true
+	return false
 end
 
 function modifier_item_heavens_halberd_datadriven_disarm:GetEffectName()
@@ -19,4 +19,15 @@ end
 
 function modifier_item_heavens_halberd_datadriven_disarm:GetEffectAttachType()
 	return PATTACH_ABSORIGIN 
+end
+
+function modifier_item_heavens_halberd_datadriven_disarm:OnCreated()
+	self:StartIntervalThink(0.1)
+end
+
+function modifier_item_heavens_halberd_datadriven_disarm:OnIntervalThink()
+	local parent = self:GetParent()
+	if parent:IsMagicImmune() then
+		self:Destroy()
+	end
 end
