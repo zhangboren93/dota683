532346d2d8d9c04d65784a56cf9017fdaa8b3054
Boren Zhang <bobo1993324@126.com>
2024-11-12 12:23:55 +0800

    Fix ember spirit death remnant fly.

--- scripts/vscripts/heroes/hero_ember_spirit/modifier_fire_remnant_counter_cooldown.lua
+++ scripts/vscripts/heroes/hero_ember_spirit/modifier_fire_remnant_counter_cooldown.lua
@@ -1,3 +1,5 @@
 modifier_fire_remnant_counter_cooldown_lua = class({
-	GetAttributes = function() return MODIFIER_ATTRIBUTE_MULTIPLE end
+	GetAttributes = function()
+		return MODIFIER_ATTRIBUTE_MULTIPLE + MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+	end
 })
