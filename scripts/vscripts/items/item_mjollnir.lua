9ae3c55cd8f67d055a04cd9379a66f57f5bf6922
Boren Zhang <bobo1993324@gmail.com>
2024-11-13 22:35:38 +0800

    static shield on damage taken, no self damage & hp loss, and randomly pick units.

--- scripts/vscripts/items/item_mjollnir.lua
+++ scripts/vscripts/items/item_mjollnir.lua
@@ -1,38 +1,56 @@
 function shield_triggered(event)
-    local attacker = event.attacker
-    local target = event.target
-    local ability = event.ability
-    local damage = ability:GetSpecialValueFor("static_damage")
-    local static_radius = ability:GetSpecialValueFor("static_range")
-    local count = ability:GetSpecialValueFor("static_count") - 1
+	local attacker = event.attacker
+	local target = event.target
+	local ability = event.ability
+	local damage = ability:GetSpecialValueFor("static_damage")
+	local static_radius = ability:GetSpecialValueFor("static_range")
+	local count = ability:GetSpecialValueFor("static_count") - 1
 
-    local time = GameRules:GetGameTime()
-    if attacker:IsBuilding() or attacker:IsMagicImmune() then
-        return
-    end
-    if ability.staticTime == nil or time - ability.staticTime > 1 then
-        ability.staticTime = time
+	local time = GameRules:GetGameTime()
+	if attacker:IsBuilding() then
+		return
+	end
+	if ability.staticTime == nil or time - ability.staticTime > 1 then
+		ability.staticTime = time
 
-        local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
-        ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))
-        ParticleManager:SetParticleControl(particle,1,Vector(attacker:GetAbsOrigin().x,attacker:GetAbsOrigin().y,attacker:GetAbsOrigin().z + attacker:GetBoundingMaxs().z ))   
-        ApplyDamage({ victim = attacker, attacker = ability:GetCaster(), damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
+		if not attacker:IsMagicImmune() then
+			local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
+			ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))
+			ParticleManager:SetParticleControl(particle,1,Vector(attacker:GetAbsOrigin().x,attacker:GetAbsOrigin().y,attacker:GetAbsOrigin().z + attacker:GetBoundingMaxs().z ))   
+			ApplyDamage({ victim = attacker, attacker = ability:GetCaster(), damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
+		end
 
-		local units = FindUnitsInRadius(target:GetTeamNumber(), target:GetAbsOrigin(), nil, static_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, FIND_ANY_ORDER, false)
-        for i=1,#units do
-            if units[i] ~= attacker and not units[i]:IsBuilding() and not units[i]:IsMagicImmune() then
-                local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
-                ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))
-                ParticleManager:SetParticleControl(particle,1,Vector(units[i]:GetAbsOrigin().x,units[i]:GetAbsOrigin().y,units[i]:GetAbsOrigin().z + units[i]:GetBoundingMaxs().z ))   
-                ApplyDamage({ victim = units[i], attacker = ability:GetCaster(), damage = damage, damage_type = DAMAGE_TYPE_MAGICAL })
-                count = count - 1
-                if count == 0 then
-                    break
-                end
-            end
-        end
-        target:EmitSound("Item.Maelstrom.Chain_Lightning.Jump")
-    end
+		local units = FindUnitsInRadius(target:GetTeamNumber(), target:GetAbsOrigin(), nil, 
+				static_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, 
+				DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0,
+				FIND_ANY_ORDER, false)
+		-- randomly pick units
+		local filtered_units = {}
+		for i=1,#units do
+			if units[i] ~= attacker and not units[i]:IsBuilding() and not units[i]:IsMagicImmune() then
+				table.insert(filtered_units, units[i])
+			end
+		end
+		if #filtered_units > count then
+			local randomed_units = {}
+			for i=1,count do
+				idx = RandomInt(1, #filtered_units)
+				table.insert(randomed_units, filtered_units[idx])
+				table.remove(filtered_units, idx)
+			end
+			units = randomed_units
+		else
+			units = filtered_units
+		end
+
+		for i=1,#units do
+			local particle = ParticleManager:CreateParticle( "particles/items_fx/chain_lightning.vpcf", PATTACH_POINT_FOLLOW, target )
+			ParticleManager:SetParticleControl(particle,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))
+			ParticleManager:SetParticleControl(particle,1,Vector(units[i]:GetAbsOrigin().x,units[i]:GetAbsOrigin().y,units[i]:GetAbsOrigin().z + units[i]:GetBoundingMaxs().z ))   
+			ApplyDamage({ victim = units[i], attacker = ability:GetCaster(), damage = damage, damage_type = DAMAGE_TYPE_MAGICAL })
+		end
+		target:EmitSound("Item.Maelstrom.Chain_Lightning.Jump")
+	end
 end
 
 item_mjollnir_datadriven = class({})
--- scripts/vscripts/items/modifier_item_mjollnir_shield_datadriven.lua
+++ scripts/vscripts/items/modifier_item_mjollnir_shield_datadriven.lua
@@ -5,16 +5,22 @@ function modifier_item_mjollnir_shield_datadriven:GetEffectName()
 end
 function modifier_item_mjollnir_shield_datadriven:DeclareFunctions()
 	return {
-		MODIFIER_EVENT_ON_ATTACKED 
+		MODIFIER_EVENT_ON_TAKEDAMAGE 
 	}
 end
-function modifier_item_mjollnir_shield_datadriven:OnAttacked(event)
-	if self:GetParent() ~= event.target then return end
+function modifier_item_mjollnir_shield_datadriven:OnTakeDamage(event)
+	local parent = self:GetParent()
+	if parent ~= event.unit then return end
+	local attacker = event.attacker
+	if attacker == parent then return end
+	if event.damage < 5 then return end
+	if bit.band(event.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) ~= 0 then return end
 	local ability = self:GetAbility()
 	local static_chance = ability:GetSpecialValueFor("static_chance")
 	if RandomInt(1, 100) <= static_chance then
 		event.ability = ability
 		event.caster = self:GetCaster()
+		event.target = event.unit
 		shield_triggered(event)
 	end
 end
