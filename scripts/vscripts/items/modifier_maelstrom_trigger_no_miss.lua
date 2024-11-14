b5c729439e1e7777379bde94632b8ba588aa60b6
Boren Zhang <bobo1993324@126.com>
2024-11-14 22:49:25 +0800

    Maelstrom can miss

--- scripts/vscripts/items/modifier_maelstrom_trigger_no_miss.lua
+++ scripts/vscripts/items/modifier_maelstrom_trigger_no_miss.lua
@@ -1,16 +1,20 @@
 require("scripts/vscripts/items/item_maelstrom")
 modifier_maelstrom_trigger_no_miss = class({})
-function modifier_maelstrom_trigger_no_miss:CheckState()
-	return {
-		[MODIFIER_STATE_CANNOT_MISS] = true
-	}
-end
 function modifier_maelstrom_trigger_no_miss:DeclareFunctions()
 	return {
-		MODIFIER_EVENT_ON_ATTACK_LANDED
+		MODIFIER_EVENT_ON_ATTACK_LANDED,
+		MODIFIER_EVENT_ON_ATTACK_FAIL 
 	}
 end
+
 function modifier_maelstrom_trigger_no_miss:OnAttackLanded(event)
+	if event.attacker ~= self:GetParent() then return end
+	event.ability = self:GetAbility()
+	event.caster = self:GetCaster()
+	modifier_item_maelstrom_datadriven_on_orb_impact(event)
+end
+
+function modifier_maelstrom_trigger_no_miss:OnAttackFail(event)
 	if event.attacker ~= self:GetParent() then return end
 	event.ability = self:GetAbility()
 	event.caster = self:GetCaster()
