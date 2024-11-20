47a4d05c51d9bd0e9447cf4905faa1f1d5305eff
Boren Zhang <bobo1993324@126.com>
2024-11-20 12:40:21 +0800

    tidebringer compare attack point and cooldown remaing.

--- scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
+++ scripts/vscripts/heroes/hero_kunkka/tidebringer.lua
@@ -19,3 +19,23 @@ function handleAttack(event)
 		end
 	end, "Adds tidebringer effect.", 0.1)
 end
+
+function handleAttackStart(event)
+	local caster = event.caster
+	local ability = event.ability
+	if ability:IsCooldownReady() then
+		if not caster:HasModifier("modifier_tidebringer_cleave") and not caster:IsAttacking() then
+			caster:AddNewModifier(caster, ability, "modifier_tidebringer_cleave", {})
+		end
+		return
+	end
+	local remaining = ability:GetCooldownTimeRemaining()
+	local attack_speed = caster:GetAttackSpeed(false)
+	local attack_point = caster:GetAttackAnimationPoint()  / attack_speed
+	--print("as:" .. attack_speed ..",ap:" .. attack_point .. ",rm:"..remaining)
+	if remaining <= attack_point then
+		if not caster:HasModifier("modifier_tidebringer_cleave") then
+			caster:AddNewModifier(caster, ability, "modifier_tidebringer_cleave", {})
+		end
+	end
+end
