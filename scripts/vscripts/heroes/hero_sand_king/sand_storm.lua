52f9e21bad0e385645d63f1c52644efddac50fca
Boren Zhang <bobo1993324@126.com>
2024-12-01 08:43:30 +0800

    sandstorm fix consecutive cast.

--- scripts/vscripts/heroes/hero_sand_king/sand_storm.lua
+++ scripts/vscripts/heroes/hero_sand_king/sand_storm.lua
@@ -24,13 +24,26 @@ function handleSpellStart(keys)
 		ability:ApplyDataDrivenModifier(caster, unit, "modifier_sand_storm_vfx_datadriven", {})
 		unit:EmitSound("Ability.SandKing_SandStorm.loop")	
 	end)
+
 end
 
 function handleDelayedRemove(event)
 	local caster = event.caster
 	local ability = event.ability 
+	if caster:HasModifier("modifier_sand_storm_debuff_datadriven") then
+		return 
+	end
 	if ability.sand_storm_unit ~= nil then
 		ability.sand_storm_unit:ForceKill(false)
 		ability.sand_storm_unit = nil
 	end
+	caster:RemoveModifierByName("modifier_invisible")
+end
+
+function handleAbilityExecuted(event)
+	local target_ability = event.target_ability
+	if target_ability:GetName() == "sandking_sand_storm_datadriven" then return end
+	local caster = event.caster
+	caster:RemoveModifierByName("modifier_sand_storm_debuff_datadriven")
+	caster:RemoveModifierByName("modifier_invisible")
 end
