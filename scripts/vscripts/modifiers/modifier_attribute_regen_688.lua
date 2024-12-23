bf7375071694233cc7c05e7008259df42310edb2
Violet Nonbloosom <11645833+violet-nonbloosom@user.noreply.gitee.com>
2024-12-23 08:57:48 +0800

    688g spell amplify from intelligence.

--- scripts/vscripts/modifiers/modifier_attribute_regen_688.lua
+++ scripts/vscripts/modifiers/modifier_attribute_regen_688.lua
@@ -2,9 +2,11 @@ modifier_attribute_regen_688_lua = class({
 	GetAttributes = function() return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end,
 	DeclareFunctions = function() return {
 		MODIFIER_PROPERTY_HEALTH_BONUS,
-		MODIFIER_PROPERTY_MANA_BONUS
+		MODIFIER_PROPERTY_MANA_BONUS,
+		MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE
 	} end,
 	GetModifierHealthBonus = function(self) return 50 + self:GetParent():GetStrength() end,
 	GetModifierManaBonus = function(self) return 50 - self:GetParent():GetIntellect(false) end,
+	GetModifierSpellAmplify_Percentage = function(self) return self:GetParent():GetIntellect(false) / 16.0 end,
 	IsHidden = function() return true end
 })
