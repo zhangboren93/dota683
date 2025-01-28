24c832f9aff956eb3f23b52da4ed4bf7f2260853
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-01-28 17:18:11 +0800

    Base Health & Mana move to bash script.

--- scripts/vscripts/modifiers/modifier_attribute_regen_688.lua
+++ scripts/vscripts/modifiers/modifier_attribute_regen_688.lua
@@ -6,8 +6,8 @@ modifier_attribute_regen_688_lua = class({
 		MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
 		MODIFIER_PROPERTY_ATTACK_RANGE_BASE_OVERRIDE
 	} end,
-	GetModifierHealthBonus = function(self) return 50 + self:GetParent():GetStrength() end,
-	GetModifierManaBonus = function(self) return 50 - self:GetParent():GetIntellect(false) end,
+	GetModifierHealthBonus = function(self) return self:GetParent():GetStrength() end,
+	GetModifierManaBonus = function(self) return 0 - self:GetParent():GetIntellect(false) end,
 	GetModifierSpellAmplify_Percentage = function(self) return self:GetParent():GetIntellect(false) / 16.0 end,
 	GetModifierAttackRangeOverride = function(self) if not self:GetParent():IsRangedAttacker() then return 150 end end,
 	IsHidden = function() return true end
