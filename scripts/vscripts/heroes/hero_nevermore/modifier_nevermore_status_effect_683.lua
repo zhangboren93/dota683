a7516f153bdc8f34c561ef9669d240fb2c218028
Boren Zhang <bobo1993324@126.com>
2025-03-10 13:12:12 +0800

    sf add custom projectile.

--- scripts/vscripts/heroes/hero_nevermore/modifier_nevermore_status_effect_683.lua
+++ scripts/vscripts/heroes/hero_nevermore/modifier_nevermore_status_effect_683.lua
@@ -4,20 +4,19 @@ modifier_nevermore_status_effect_683_lua = class({
 		if data.style ~= nil and parent ~= nil then
 			print("modifier_sf_weapon_effect_683_lua creates with " .. data.style)
 			if data.style == "deft" then
-				self.particleId = ParticleManager:CreateParticle("particles/units/heroes/hero_nevermore/nevermore_trail_default.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
-			elseif data.style == "whit" then
-				self.particleId = ParticleManager:CreateParticle("particles/units/heroes/hero_nevermore/nevermore_trail_custom.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
-				ParticleManager:SetParticleControl(self.particleId, 15, Vector(255, 255, 255))
-				ParticleManager:SetParticleControl(self.particleId, 16, Vector(1, 0, 0))
+				return
+			elseif data.style == "orange" then
+				self.mpn = "particles/units/heroes/hero_nevermore/sf_necromastery_attack.vpcf"
+			elseif data.style == "red" then
+				self.mpn = "particles/units/heroes/hero_nevermore/sf_necromastery_attack_pink.vpcf"
 			end
-			--ParticleManager:SetParticleControlEnt(self.particleId, 0, parent, PATTACH_ABSORIGIN_FOLLOW, "attach_hitloc", Vector(0, 0, 0), true)
 		end
 	end,
 	GetAttributes = function() return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end,
-	OnDestroy = function(self)
-		if self.particleId ~= nil then
-			ParticleManager:DestroyParticle(self.particleId, false)
-		end
-	end,
-	IsHidden = function() return true end
+	IsHidden = function() return true end,
+	DeclareFunctions = function() return { MODIFIER_PROPERTY_PROJECTILE_NAME } end,
+	GetModifierProjectileName = function(self)
+		return self.mpn
+	--	return "particles/units/heroes/hero_nevermore/sf_necromastery_attack.vpcf"
+	end
 })
