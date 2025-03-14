270aec30a4b729ec8490e738275cd73e9cbee9d4
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-03-14 17:28:22 +0800

    6.88 manta

--- scripts/vscripts/items/item_manta.lua
+++ scripts/vscripts/items/item_manta.lua
@@ -3,7 +3,7 @@ function handleAbilityExecuted(event)
 	local caster = event.caster
 	if target_ability:GetName() == "item_manta" and caster:IsRangedAttacker() then
 		caster:SetThink(function()
-			target_ability:StartCooldown(50)
+			target_ability:StartCooldown(target_ability:GetSpecialValueFor("cooldown_ranged"))
 		end, "set cooldown for ranged hero", 0.1)
 	end
 end
