351495ab941e176c8be2b132e13388ae4ae1ec66
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-03-25 08:44:59 +0800

    fire spirits fix

--- scripts/vscripts/heroes/hero_phoenix/fire_spirits.lua
+++ scripts/vscripts/heroes/hero_phoenix/fire_spirits.lua
@@ -1,30 +1,10 @@
---[[
-	Author: Noya
-	Date: 16.01.2015.
-	Levels up the ability_name to the same level of the ability that runs this
-]]
-function LevelUpAbility( event )
-	local caster = event.caster
-	local this_ability = event.ability		
-	local this_abilityName = this_ability:GetAbilityName()
-	local this_abilityLevel = this_ability:GetLevel()
-
-	-- The ability to level up
-	local ability_name = event.ability_name
-	local ability_handle = caster:FindAbilityByName(ability_name)	
-	if ability_handle == nil then return end
-	local ability_level = ability_handle:GetLevel()
-
-	-- Check to not enter a level up loop
-	if ability_level ~= this_abilityLevel then
-		ability_handle:SetLevel(this_abilityLevel)
-	end
-end
-
 phoenix_fire_spirits_lua = class({
 	GetAssociatedSecondaryAbilities = function() return "phoenix_launch_fire_spirit_lua" end,
 	ProcsMagicStick = function() return true end,
+	GetHealthCost = function(self) return self:GetCaster():GetHealth() * self:GetSpecialValueFor("hp_cost_perc") / 100 end,
 	OnSpellStart = function(self)
+		if not IsServer() then return end
+
 		local caster = self:GetCaster()
 		local ability = self
 		local duration = self:GetSpecialValueFor("spirit_duration")
@@ -35,6 +15,7 @@ phoenix_fire_spirits_lua = class({
 		local sub_ability_name	= "phoenix_launch_fire_spirit_lua"
 		local main_ability_name	= self:GetAbilityName()
 		caster:EmitSound("Hero_Phoenix.FireSpirits.Cast")
+		caster:EmitSoundParams( "Hero_Phoenix.FireSpirits.Loop", 100, 0.0, self:GetSpecialValueFor( "spirit_duration" ) )
 		caster:AddNewModifier(caster, self, modifierStackName, { duration = duration })
 
 		-- Create particle FX
@@ -54,10 +35,10 @@ phoenix_fire_spirits_lua = class({
 		-- Set the stack count
 		caster:SetModifierStackCount( modifierStackName, ability, numSpirits )
 
-		caster:SetHealth( caster:GetHealth() * ( 100 - hpCost ) / 100 )
-
 		-- Swap sub ability
 		caster:SwapAbilities( main_ability_name, sub_ability_name, false, true )
+
+		caster:StartGesture( ACT_DOTA_CAST_ABILITY_2 )
 	end,
 	OnUpgrade = function(self)
 		local caster = self:GetCaster()
