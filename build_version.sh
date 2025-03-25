35d44c8f97ec61032a12cf42082faa1b631640d3
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-03-25 22:37:50 +0800

    split conf into hero conf and item conf

--- build_version.sh
+++ build_version.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 cp scripts/npc/npc_heroes_custom.txt.template scripts/npc/npc_heroes_custom.txt
 cp scripts/npc/npc_items_custom.txt.template scripts/npc/npc_items_custom.txt
+
+str="_heroes.conf"
+echo "HEROES : "
 while IFS= read -r line; do
 	readarray -d = -t KVS <<< $line
 	KEY=${KVS[0]}
 	VAL=`echo ${KVS[1]} | tr -d '\n'`
 	echo "s/%$KEY%/$VAL/g"
 	sed -i "s/%$KEY%/$VAL/g" scripts/npc/npc_heroes_custom.txt
+done < $1$str
+
+str="_version.conf"
+echo "ITEMS : "
+while IFS= read -r line; do
+	readarray -d = -t KVS <<< $line
+	KEY=${KVS[0]}
+	VAL=`echo ${KVS[1]} | tr -d '\n'`
+	echo "s/%$KEY%/$VAL/g"
 	sed -i "s/%$KEY%/$VAL/g" scripts/npc/npc_items_custom.txt
-done < $1
+done < $1$str
