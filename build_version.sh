d5eeb2add07ea20d4b14a96a349690aa2141fb7f
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-03-26 09:06:34 +0800

    abilities template

--- build_version.sh
+++ build_version.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+cp scripts/npc/npc_abilities_custom.txt.template scripts/npc/npc_abilities_custom.txt
 cp scripts/npc/npc_heroes_custom.txt.template scripts/npc/npc_heroes_custom.txt
 cp scripts/npc/npc_items_custom.txt.template scripts/npc/npc_items_custom.txt
+
+str="_abilities.conf"
+echo "ABILITIES : "
+while IFS= read -r line; do
+	readarray -d = -t KVS <<< $line
+	KEY=${KVS[0]}
+	VAL=`echo ${KVS[1]} | tr -d '\n'`
+	echo "s/%$KEY%/$VAL/g"
+	sed -i "s/%$KEY%/$VAL/g" scripts/npc/npc_abilities_custom.txt
+done < $1$str
 
 str="_heroes.conf"
 echo "HEROES : "
