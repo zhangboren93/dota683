6b6ce6da851106549f55dcb842b22020dca06a37
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-04-16 22:52:06 +0800

    merge files about heart

--- scripts/vscripts/items/item_heart.lua
+++ scripts/vscripts/items/item_heart.lua
@@ -50,3 +50,15 @@ end
 function modifier_item_heart_datadriven_regen_on_destroy(keys)
 	keys.caster:RemoveModifierByName("modifier_item_heart_regen_lua")
 end
+
+modifier_item_heart_regen_lua = class({})
+
+function modifier_item_heart_regen_lua:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT
+	}
+end
+
+function modifier_item_heart_regen_lua:GetModifierConstantHealthRegen()
+	return self:GetAbility():GetSpecialValueFor("health_regen_percent_per_second") * self:GetParent():GetMaxHealth() / 100
+end
