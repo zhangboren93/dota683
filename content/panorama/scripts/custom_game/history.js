13eb42b3f68b94bfe302eded3e3bc85c4990f10f
Boren Zhang <bobo1993324@gmail.com>
2025-04-18 00:18:56 +0800

    Adjust styles.

--- content/panorama/scripts/custom_game/history.js
+++ content/panorama/scripts/custom_game/history.js
@@ -10,20 +10,49 @@ function OnHistoryPanelScores(data) {
 	let team = DOTATeam_t.DOTA_TEAM_GOODGUYS
 	let pref = "r";
 	let radi_players = Game.GetPlayerIDsOnTeam(team);
-	for (int i = 0; i < radi_players.length; i++) {
+	for (let i = 0; i < radi_players.length; i++) {
 		let pid = radi_players[i]
 		if (pid >= data.length) {
 			continue;
 		}
 		let player_info = Game.GetPlayerInfo(pid)
 		$.Msg(player_info)
+		let steamid = player_info["player_steamid"];
 		let hero = Players.GetPlayerSelectedHero(pid)
-		let score = radi_players[i][0]
-		let game = radi_players[i][1] + radi_players[i][2]
-		let winr = Math.floor(radi_players[i][1] * 100 / game) + "%";
-    	$("#"+pref+"score"+(i+1)).text = score;
+		let score = data[pid+1]['1']
+		let game = parseInt(data[pid+1]['2']) + parseInt(data[pid+1]['3'])
     	$("#"+pref+"count"+(i+1)).text = game;
+		if (game == 0) {
+			game = 1;
+		}
+		let winr = Math.floor(parseInt(data[pid+1]['2']) * 100 / game) + "%";
+    	$("#"+pref+"score"+(i+1)).text = score;
     	$("#"+pref+"winr" +(i+1)).text = winr;
 		$("#"+pref+"dhi"  +(i+1)).heroname = hero;
+		$("#"+pref+"dun"  +(i+1)).steamid = steamid;
+	}
+	team = DOTATeam_t.DOTA_TEAM_BADGUYS;
+	pref = "d";
+	radi_players = Game.GetPlayerIDsOnTeam(team);
+	for (let i = 0; i < radi_players.length; i++) {
+		let pid = radi_players[i]
+		if (pid >= data.length) {
+			continue;
+		}
+		let player_info = Game.GetPlayerInfo(pid)
+		$.Msg(player_info)
+		let steamid = player_info["player_steamid"];
+		let hero = Players.GetPlayerSelectedHero(pid)
+		let score = data[pid+1]['1']
+		let game = parseInt(data[pid+1]['2']) + parseInt(data[pid+1]['3'])
+    	$("#"+pref+"count"+(i+1)).text = game;
+		if (game == 0) {
+			game = 1;
+		}
+		let winr = Math.floor(parseInt(data[pid+1]['2']) * 100 / game) + "%";
+    	$("#"+pref+"score"+(i+1)).text = score;
+    	$("#"+pref+"winr" +(i+1)).text = winr;
+		$("#"+pref+"dhi"  +(i+1)).heroname = hero;
+		$("#"+pref+"dun"  +(i+1)).steamid = steamid;
 	}
 }
