7d422adfa35caa3962b87afbe7f0416bb0bdcf0a
Boren Zhang <bobo1993324@126.com>
2025-04-19 11:28:59 +0800

    Fix hero ping miss.

--- content/panorama/scripts/custom_game/hero_bar_block.js
+++ content/panorama/scripts/custom_game/hero_bar_block.js
@@ -1,10 +1,18 @@
 function handleHeroBarBlockClick(num) {
 	$.Msg("handleHeroBarBlockClick " + num);
 	if (GameUI.IsAltDown()) {
-		GameEvents.SendCustomGameEventToServer("custom_ping_hero_missing", { num: num, snd: Players.GetLocalPlayer() })
+		//GameEvents.SendCustomGameEventToServer("custom_ping_hero_missing", { num: num, snd: Players.GetLocalPlayer() })
 	}
 }
 
+function HideWhenAltIsDown() {
+	if (GameUI.IsAltDown()) {
+		$("#hero-bar-block-buttons").visible = false;
+	} else {
+		$("#hero-bar-block-buttons").visible = true;
+	}
+	$.Schedule(0.1, HideWhenAltIsDown)
+}
 (function ()
 {
 	if (Players.GetTeam(Players.GetLocalPlayer()) === DOTATeam_t.DOTA_TEAM_GOODGUYS) {
@@ -14,4 +22,5 @@ function handleHeroBarBlockClick(num) {
 		$("#hero-bar-block-buttons").AddClass("radiant-block");
 		$("#hero-bar-block-buttons").visible = true
 	}
+	$.Schedule(0.1, HideWhenAltIsDown)
 })();
