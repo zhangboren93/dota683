a56c8ab9cd2ae5e562a87b9ce5f285351008b133
Boren Zhang <bobo1993324@126.com>
2025-04-20 14:58:32 +0800

    disable craggy exterier on illusions.

--- scripts/vscripts/heroes/hero_tiny/craggy_exterior.lua
+++ scripts/vscripts/heroes/hero_tiny/craggy_exterior.lua
@@ -5,7 +5,7 @@ function handleAttackStart( event )
 	local ability = event.ability
 	local percentage = ability:GetSpecialValueFor("stun_chance")
 
-	if target == caster and RandomFloat(0, 1) * 100 < percentage then
+	if target == caster and not caster:IsIllusion() and RandomFloat(0, 1) * 100 < percentage then
 		EmitSoundOn("Hero_Tiny.CraggyExterior.Stun", attacker)
 		attacker:AddNewModifier(target, ability, "modifier_stunned", {duration = ability:GetSpecialValueFor("stun_duration")})
 		ApplyDamage({
