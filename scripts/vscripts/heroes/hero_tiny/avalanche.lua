4c157ba9d9ad4db69bb61f4e0a1f58ebe56d74e0
Boren Zhang <bobo1993324@gmail.com>
2025-04-25 00:00:39 +0800

    Adjust avalanch particle.

--- scripts/vscripts/heroes/hero_tiny/avalanche.lua
+++ scripts/vscripts/heroes/hero_tiny/avalanche.lua
@@ -24,12 +24,12 @@ function handleSpellStart(event)
 	local ability = event.ability
 	local tick_damage = ability:GetSpecialValueFor("avalanche_damage") / 4
 	EmitSoundOnLocationWithCaster(target_point, "Ability.Avalanche", caster )
-	if caster.avalanche_particle == nil then
-		caster.avalanche_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_tiny/tiny_avalanche.vpcf", PATTACH_POINT, caster)
-		ParticleManager:SetParticleControl(caster.avalanche_particle, 0, target_point + Vector(132, 0, 0))
-		ParticleManager:SetParticleControl(caster.avalanche_particle, 1, Vector(275, 275, 1))
-	end
 	caster:SetThink(function()
+		if caster.avalanche_particle == nil then
+			caster.avalanche_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_tiny/tiny_avalanche.vpcf", PATTACH_POINT, caster)
+			ParticleManager:SetParticleControl(caster.avalanche_particle, 0, target_point)
+			ParticleManager:SetParticleControl(caster.avalanche_particle, 1, Vector(225, 1, 1))
+		end
 		damageStunUnits(caster, target_point, ability, tick_damage)
 	end, "avalanche delay", 0.5)
 	caster:SetThink(function()
