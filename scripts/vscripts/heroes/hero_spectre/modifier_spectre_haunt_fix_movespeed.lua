ea1d9623f81d379652ca577b96fce68f38bd74f5
Boren Zhang <bobo1993324@126.com>
2025-04-24 12:43:10 +0800

    spectre haunt movespeed adds boot.

--- scripts/vscripts/heroes/hero_spectre/modifier_spectre_haunt_fix_movespeed.lua
+++ scripts/vscripts/heroes/hero_spectre/modifier_spectre_haunt_fix_movespeed.lua
@@ -8,5 +8,24 @@ modifier_spectre_haunt_fix_movespeed_lua = class({
 		end
 	end,
 	DeclareFunctions = function() return { MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE } end,
-	GetModifierMoveSpeed_Absolute = function() return 400 end
+	GetModifierMoveSpeed_Absolute = function()
+		local boot_speed = 0
+		local parent = self:GetParent()
+		if parent ~= nil then
+			if parent:HasItemInInventory("item_travel_boots_datadriven") then
+				boot_speed = 100
+			elseif parent:HasItemInInventory("item_phase_boots") then
+				boot_speed = 50
+			elseif parent:HasItemInInventory("item_power_treads") then
+				boot_speed = 50
+			elseif parent:HasItemInInventory("item_arcane_boots") then
+				boot_speed = 55
+			elseif parent:HasItemInInventory("item_tranquil_boots_datadriven") then
+				boot_speed = 60
+			elseif parent:HasItemInInventory("item_boots") then
+				boot_speed = 50
+			end
+		end
+		return 400 + boot_speed
+	end
 })
